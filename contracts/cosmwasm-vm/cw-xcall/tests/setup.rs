109b8ca7918e5a2be2c1618af09bc470fcab3ea4
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-02-09 16:35:32 +0530

    update test

--- contracts/cosmwasm-vm/cw-xcall/tests/setup.rs
+++ contracts/cosmwasm-vm/cw-xcall/tests/setup.rs
@@ -1,6 +1,9 @@
 use cosmwasm_std::{
     coins,
-    testing::{mock_dependencies, mock_env, mock_info, MockApi, MockQuerier, MockStorage},
+    testing::{
+        mock_dependencies, mock_env, mock_info, MockApi, MockQuerier, MockStorage,
+        MOCK_CONTRACT_ADDR,
+    },
     Addr, BlockInfo, ContractInfo, Empty, Env, MessageInfo, OwnedDeps, Timestamp, TransactionInfo,
 };
 
@@ -61,7 +64,22 @@ pub fn deps() -> OwnedDeps<MockStorage, MockApi, MockQuerier, Empty> {
 fn test() {
     let mock = mock_env();
 
-    let mock_env_builder: Env = MockEnvBuilder::new().add_block(mock.block.clone()).build();
+    let block_info = BlockInfo {
+        height: 12_345,
+        time: Timestamp::from_nanos(1_571_797_419_879_305_533),
+        chain_id: "cosmos-testnet-14002".to_string(),
+    };
 
-    assert_eq!(mock.block, mock_env_builder.block)
+    let transaction = Some(TransactionInfo { index: 3 });
+    let contract = ContractInfo {
+        address: Addr::unchecked(MOCK_CONTRACT_ADDR),
+    };
+
+    let mock_env_builder: Env = MockEnvBuilder::new()
+        .add_block(block_info)
+        .add_txn_info(transaction.unwrap())
+        .add_contract_info(contract)
+        .build();
+
+    assert_eq!(mock, mock_env_builder)
 }
