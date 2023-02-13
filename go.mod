46c155866139e47686f8c97225d70ee5040447a8
hemz10 <sph2740@example.com>
2023-02-13 15:41:00 +0530

    feat : Icon Chain implementation for IBC test

--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ require (
 	github.com/docker/docker v20.10.19+incompatible
 	github.com/docker/go-connections v0.4.0
 	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1
+	github.com/icon-project/icon-bridge v0.0.11
 	github.com/rivo/tview v0.0.0-20220307222120-9994674d60a8
+	github.com/strangelove-ventures/ibctest v0.0.0-20220912210217-eb7f6a3b4e6a
 	github.com/strangelove-ventures/interchaintest/v6 v6.0.0-20230208222759-0b3a8a07775d
 	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.23.0
@@ -39,7 +41,9 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d // indirect
 	github.com/bgentry/speakeasy v0.1.0 // indirect
+	github.com/bshuster-repo/logrus-logstash-hook v0.4.1 // indirect
 	github.com/btcsuite/btcd v0.22.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/centrifuge/go-substrate-rpc-client/v4 v4.0.10 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -51,6 +55,7 @@ require (
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/cosmos/gorocksdb v1.2.0 // indirect
 	github.com/cosmos/iavl v0.19.4 // indirect
+	github.com/cosmos/ibc-go/v5 v5.0.0-beta1 // indirect
 	github.com/cosmos/ibc-go/v6 v6.1.0 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.12.1 // indirect
 	github.com/danieljoos/wincred v1.1.2 // indirect
@@ -59,6 +64,7 @@ require (
 	github.com/decred/base58 v1.0.4 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v2 v2.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/dgraph-io/badger/v2 v2.2007.4 // indirect
 	github.com/dgraph-io/ristretto v0.1.0 // indirect
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
@@ -67,14 +73,18 @@ require (
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/dvsekhvalnov/jose2go v1.5.0 // indirect
 	github.com/ethereum/go-ethereum v1.10.20 // indirect
+	github.com/evalphobia/logrus_fluent v0.5.4 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/fluent/fluent-logger-golang v1.4.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/go-errors/errors v1.0.1 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
+	github.com/gofrs/uuid v4.3.0+incompatible // indirect
 	github.com/gogo/gateway v1.1.0 // indirect
 	github.com/gogo/protobuf v1.3.3 // indirect
 	github.com/golang/glog v1.0.0 // indirect
@@ -94,6 +104,7 @@ require (
 	github.com/gsterjov/go-libsecret v0.0.0-20161001094733-a6f4afe4910c // indirect
 	github.com/gtank/merlin v0.1.1 // indirect
 	github.com/gtank/ristretto255 v0.1.2 // indirect
+	github.com/haltingstate/secp256k1-go v0.0.0-20151224084235-572209b26df6 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-getter v1.6.1 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
@@ -103,6 +114,8 @@ require (
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hdevalence/ed25519consensus v0.0.0-20220222234857-c00d1f31bab3 // indirect
+	github.com/hugobyte/keygen v0.1.0 // indirect
+	github.com/icon-project/goloop v1.2.11 // indirect
 	github.com/icza/dyno v0.0.0-20220812133438-f0b6f8a18845 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/ipfs/go-cid v0.0.7 // indirect
@@ -111,12 +124,15 @@ require (
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/compress v1.15.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/labstack/echo/v4 v4.9.0 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
 	github.com/libp2p/go-libp2p-core v0.15.1 // indirect
 	github.com/libp2p/go-openssl v0.0.7 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
@@ -140,6 +156,7 @@ require (
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/philhofer/fwd v1.0.0 // indirect
 	github.com/pierrec/xxHash v0.1.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -152,6 +169,7 @@ require (
 	github.com/regen-network/cosmos-proto v0.3.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
 	github.com/rs/cors v1.8.2 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
@@ -170,8 +188,13 @@ require (
 	github.com/tendermint/go-amino v0.16.0 // indirect
 	github.com/tendermint/tendermint v0.34.24 // indirect
 	github.com/tendermint/tm-db v0.6.7 // indirect
+	github.com/tinylib/msgp v1.1.2 // indirect
 	github.com/ulikunitz/xz v0.5.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	github.com/vedhavyas/go-subkey v1.0.3 // indirect
+	github.com/vmihailenco/msgpack/v4 v4.3.11 // indirect
+	github.com/vmihailenco/tagparser v0.1.1 // indirect
 	github.com/zondax/hid v0.9.1 // indirect
 	github.com/zondax/ledger-go v0.14.0 // indirect
 	go.etcd.io/bbolt v1.3.6 // indirect
@@ -194,6 +217,7 @@ require (
 	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.28.2-0.20220831092852-f930b1dc76e8 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ test/chains/icon/icon_chain.go
@@ -0,0 +1,273 @@
+package icon
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"sync"
+
+	dockertypes "github.com/docker/docker/api/types"
+	volumetypes "github.com/docker/docker/api/types/volume"
+	"github.com/docker/docker/client"
+	"github.com/icon-project/IBC-Integration/test/internal/dockerutil"
+	"github.com/strangelove-ventures/ibctest/ibc"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+)
+
+type IconChain struct {
+	log           *zap.Logger
+	testName      string
+	cfg           ibc.ChainConfig
+	numValidators int
+	numFullNodes  int
+	FullNodes     IconNodes
+	findTxMu      sync.Mutex
+}
+
+func NewIconChain(testName string, chainConfig ibc.ChainConfig, numValidators int, numFullNodes int, log *zap.Logger) *IconChain {
+	return &IconChain{
+		testName:      testName,
+		cfg:           chainConfig,
+		numValidators: numValidators,
+		numFullNodes:  numFullNodes,
+		log:           log,
+	}
+}
+
+// Config fetches the chain configuration.
+func (c *IconChain) Config() ibc.ChainConfig {
+	return c.cfg
+}
+
+// Initialize initializes node structs so that things like initializing keys can be done before starting the chain
+func (c *IconChain) Initialize(ctx context.Context, testName string, cli *client.Client, networkID string) error {
+	chainCfg := c.Config()
+	c.pullImages(ctx, cli)
+	image := chainCfg.Images[0]
+
+	newFullNodes := make(IconNodes, c.numFullNodes)
+	copy(newFullNodes, c.FullNodes)
+
+	eg, egCtx := errgroup.WithContext(ctx)
+	for i := len(c.FullNodes); i < c.numFullNodes; i++ {
+		i := i
+		eg.Go(func() error {
+			fn, err := c.NewChainNode(egCtx, testName, cli, networkID, image, false)
+			if err != nil {
+				return err
+			}
+			fn.Index = i
+			newFullNodes[i] = fn
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+	c.findTxMu.Lock()
+	defer c.findTxMu.Unlock()
+	c.FullNodes = newFullNodes
+	return nil
+}
+
+func (c *IconChain) pullImages(ctx context.Context, cli *client.Client) {
+	for _, image := range c.Config().Images {
+		rc, err := cli.ImagePull(
+			ctx,
+			image.Repository+":"+image.Version,
+			dockertypes.ImagePullOptions{},
+		)
+		if err != nil {
+			c.log.Error("Failed to pull image",
+				zap.Error(err),
+				zap.String("repository", image.Repository),
+				zap.String("tag", image.Version),
+			)
+		} else {
+			_, _ = io.Copy(io.Discard, rc)
+			_ = rc.Close()
+		}
+	}
+}
+
+func (c *IconChain) NewChainNode(
+	ctx context.Context,
+	testName string,
+	cli *client.Client,
+	networkID string,
+	image ibc.DockerImage,
+	validator bool,
+) (*IconNode, error) {
+	// Construct the ChainNode first so we can access its name.
+	// The ChainNode's VolumeName cannot be set until after we create the volume.
+	in := &IconNode{
+		log:          c.log,
+		Chain:        c,
+		DockerClient: cli,
+		NetworkID:    networkID,
+		TestName:     testName,
+		Image:        image,
+	}
+
+	v, err := cli.VolumeCreate(ctx, volumetypes.VolumeCreateBody{
+		Labels: map[string]string{
+			dockerutil.CleanupLabel: testName,
+
+			dockerutil.NodeOwnerLabel: in.Name(),
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("creating volume for chain node: %w", err)
+	}
+	in.VolumeName = v.Name
+
+	if err := dockerutil.SetVolumeOwner(ctx, dockerutil.VolumeOwnerOptions{
+		Log: c.log,
+
+		Client: cli,
+
+		VolumeName: v.Name,
+		ImageRef:   image.Ref(),
+		TestName:   testName,
+		UidGid:     image.UidGid,
+	}); err != nil {
+		return nil, fmt.Errorf("set volume owner: %w", err)
+	}
+	return in, nil
+}
+
+// Start sets up everything needed (validators, gentx, fullnodes, peering, additional accounts) for chain to start from genesis.
+func (c *IconChain) Start(testName string, ctx context.Context, additionalGenesisWallets ...ibc.WalletAmount) error {
+	c.findTxMu.Lock()
+	defer c.findTxMu.Unlock()
+	eg, egCtx := errgroup.WithContext(ctx)
+	for _, n := range c.FullNodes {
+		n := n
+		eg.Go(func() error {
+			if err := n.CreateNodeContainer(egCtx); err != nil {
+				return err
+			}
+			// All (validators, gentx, fullnodes, peering, additional accounts) are included in the image itself.
+			return n.StartContainer(ctx)
+		})
+	}
+	return eg.Wait()
+}
+
+// Exec runs an arbitrary command using Chain's docker environment.
+// Whether the invoked command is run in a one-off container or execing into an already running container
+// is up to the chain implementation.
+//
+// "env" are environment variables in the format "MY_ENV_VAR=value"
+func (c *IconChain) Exec(ctx context.Context, cmd []string, env []string) (stdout []byte, stderr []byte, err error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// ExportState exports the chain state at specific height.
+func (c *IconChain) ExportState(ctx context.Context, height int64) (string, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// GetRPCAddress retrieves the rpc address that can be reached by other containers in the docker network.
+func (c *IconChain) GetRPCAddress() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// GetGRPCAddress retrieves the grpc address that can be reached by other containers in the docker network.
+func (c *IconChain) GetGRPCAddress() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// GetHostRPCAddress returns the rpc address that can be reached by processes on the host machine.
+// Note that this will not return a valid value until after Start returns.
+func (c *IconChain) GetHostRPCAddress() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// GetHostGRPCAddress returns the grpc address that can be reached by processes on the host machine.
+// Note that this will not return a valid value until after Start returns.
+func (c *IconChain) GetHostGRPCAddress() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// HomeDir is the home directory of a node running in a docker container. Therefore, this maps to
+// the container's filesystem (not the host).
+func (c *IconChain) HomeDir() string {
+	panic("not implemented") // TODO: Implement
+}
+
+// CreateKey creates a test key in the "user" node (either the first fullnode or the first validator if no fullnodes).
+func (c *IconChain) CreateKey(ctx context.Context, keyName string) error {
+	panic("not implemented") // TODO: Implement
+}
+
+// RecoverKey recovers an existing user from a given mnemonic.
+func (c *IconChain) RecoverKey(ctx context.Context, name string, mnemonic string) error {
+	panic("not implemented") // TODO: Implement
+}
+
+// GetAddress fetches the bech32 address for a test key on the "user" node (either the first fullnode or the first validator if no fullnodes).
+func (c *IconChain) GetAddress(ctx context.Context, keyName string) ([]byte, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// SendFunds sends funds to a wallet from a user account.
+func (c *IconChain) SendFunds(ctx context.Context, keyName string, amount ibc.WalletAmount) error {
+	panic("not implemented") // TODO: Implement
+}
+
+// SendIBCTransfer sends an IBC transfer returning a transaction or an error if the transfer failed.
+func (c *IconChain) SendIBCTransfer(ctx context.Context, channelID string, keyName string, amount ibc.WalletAmount, timeout *ibc.IBCTimeout) (ibc.Tx, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// UpgradeProposal submits a software-upgrade proposal to the chain.
+func (c *IconChain) UpgradeProposal(ctx context.Context, keyName string, prop ibc.SoftwareUpgradeProposal) (ibc.SoftwareUpgradeTx, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// InstantiateContract takes a file path to smart contract and initialization message and returns the instantiated contract address.
+func (c *IconChain) InstantiateContract(ctx context.Context, keyName string, amount ibc.WalletAmount, fileName string, initMessage string, needsNoAdminFlag bool) (string, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// ExecuteContract executes a contract transaction with a message using it's address.
+func (c *IconChain) ExecuteContract(ctx context.Context, keyName string, contractAddress string, message string) error {
+	panic("not implemented") // TODO: Implement
+}
+
+// DumpContractState dumps the state of a contract at a block height.
+func (c *IconChain) DumpContractState(ctx context.Context, contractAddress string, height int64) (*ibc.DumpContractStateResponse, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// CreatePool creates a balancer pool.
+func (c *IconChain) CreatePool(ctx context.Context, keyName string, contractAddress string, swapFee float64, exitFee float64, assets []ibc.WalletAmount) error {
+	panic("not implemented") // TODO: Implement
+}
+
+// Height returns the current block height or an error if unable to get current height.
+func (c *IconChain) Height(ctx context.Context) (uint64, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// GetBalance fetches the current balance for a specific account address and denom.
+func (c *IconChain) GetBalance(ctx context.Context, address string, denom string) (int64, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// GetGasFeesInNativeDenom gets the fees in native denom for an amount of spent gas.
+func (c *IconChain) GetGasFeesInNativeDenom(gasPaid int64) int64 {
+	panic("not implemented") // TODO: Implement
+}
+
+// Acknowledgements returns all acknowledgements in a block at height.
+func (c *IconChain) Acknowledgements(ctx context.Context, height uint64) ([]ibc.PacketAcknowledgement, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+// Timeouts returns all timeouts in a block at height.
+func (c *IconChain) Timeouts(ctx context.Context, height uint64) ([]ibc.PacketTimeout, error) {
+	panic("not implemented") // TODO: Implement
+}
