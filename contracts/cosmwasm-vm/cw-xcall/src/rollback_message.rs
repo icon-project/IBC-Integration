ec0324c129e4e868d5b99c6c43fbba06b2403a5c
Prathiksha-Nataraja <prathikshamn28@gmail.com>
2023-02-14 16:08:34 +0530

    Initial commit

--- contracts/cosmwasm-vm/cw-xcall/src/rollback_message.rs
+++ contracts/cosmwasm-vm/cw-xcall/src/rollback_message.rs
@@ -1,12 +1,15 @@
  
+use std::ptr::null;
 use serde::{Serialize,Deserialize, __private::de::IdentifierDeserializer};
 use schemars::JsonSchema;
-use cosmwasm_std::Event;
-use std::str::from_utf8;
+use cosmwasm_std::{Event, Never, from_binary,IbcMsg, StdError};
+use cosmwasm_std::{DepsMut,Deps,Env,IbcBasicResponse,IbcPacketReceiveMsg,IbcReceiveResponse,IbcChannel,IbcPacketTimeoutMsg,IbcPacketAckMsg,IbcExecuteMsg};
+
+use crate::{ContractError };
 
  #[derive(Serialize, Deserialize,Clone, Debug, PartialEq, Eq, JsonSchema)]
  pub struct RollbackMessage{
-    sn : u128,
+    sn : i128,
     rollback : Vec<u8>,
     message : String,
  }
@@ -20,9 +23,50 @@ use std::str::from_utf8;
     }
  }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg{
+   ExecuteRollBack{sn : i128}
+}
+
+fn do_ibc_packet_receive(
+   deps: DepsMut,
+    _env: Env,
+    msg: IbcPacketReceiveMsg,
+)-> Result<IbcReceiveResponse, ContractError>{
+   let msg: IbcExecuteMsg = from_binary(&msg.packet.data)?;
+
+    match msg {
+        IbcExecuteMsg::Event{sn,rollback,message} => execute_rollback(sn),
+    }
+}
+
+fn execute_rollback(sn: i128) -> Result<IbcReceiveResponse, ContractError> {
+let count  = try_rollback(sn)?;
+Ok(IbcReceiveResponse::new()
+.add_attribute("method","execute_rollback"))
+.add_attribute("count", count.to_string())
+.set_ack(make_ack_success()))
 
 
+// Some(count)
 
+}
+
+pub fn try_rollback(sn:i128) -> Option<i128> {
+  if sn.CallMessage(null::<_>) {
+   return None
+  } 
+  else {
+      Some(sn)
+  }
+//   let DApp = || -> Result<(),StdError> Ok({
+//    DApp::handleCallMessage().BtpAddress().get();
+//   })
+   }
+
+  
+   
 
  
 
