43b5077df885daf591adb1053664eb7e1cbb171f
Prathiksha-Nataraja <prathikshamn28@gmail.com>
2023-02-17 16:31:25 +0530

    updated testcase

--- contracts/cosmwasm-vm/cw-xcall/tests/test_rollback_message.rs
+++ contracts/cosmwasm-vm/cw-xcall/tests/test_rollback_message.rs
@@ -1,3 +1,4 @@
+use cosmwasm_std::Event;
 use cw_xcall::{
     rollback_message::{try_rollbackexecuted, RollbackMessage},
     types::address::Address,
@@ -8,23 +9,21 @@ use cw_xcall::{
 fn test_rollbackmessage() {
     let msg = RollbackMessage::new(123, vec![1, 2, 3], "message".to_owned());
     let event = msg.rollbackmessage();
-    assert_eq!(event.ty, "rollbackmessage");
-    assert_eq!(event.attributes[0].key, "sn");
-    assert_eq!(event.attributes[0].value, "123");
+    let actual = Event::new("rollbackmessage")
+        .add_attribute("sequence_no", "123")
+        .add_attribute("rollback", String::from_utf8(vec![1, 2, 3]).unwrap());
+    assert_eq!(event, actual)
 }
 
 #[test]
 fn test_rollbackexecuted() {
     let msg = RollbackMessage::new(123, vec![1, 2, 3], "message".to_owned());
     let event = msg.rollbackexecuted();
-    assert_eq!(event.ty, "rollbackexecuted");
-    assert_eq!(event.attributes.len(), 3);
-    assert_eq!(event.attributes[0].key, "sn");
-    assert_eq!(event.attributes[0].value, "123");
-    assert_eq!(event.attributes[1].key, "rollback");
-    assert_eq!(event.attributes[1].value, "\u{1}\u{2}\u{3}");
-    assert_eq!(event.attributes[2].key, "message");
-    assert_eq!(event.attributes[2].value, "message");
+    let actual = Event::new("rollbackexecuted")
+        .add_attribute("sequence_no", "123")
+        .add_attribute("rollback", String::from_utf8(vec![1, 2, 3]).unwrap())
+        .add_attribute("message", "message");
+    assert_eq!(event, actual);
 }
 
 #[test]
@@ -50,8 +49,8 @@ fn test_not_contains_rollback() {
         ),
         "88bd05442686be0a5df7da33b6f1089ebfea3769b19dbb2477fe0cd6e0f123t7".to_owned(),
         123,
+        vec![],
         vec![1, 2, 3],
-        vec![4, 5, 6],
     );
     let n = try_rollbackexecuted(msg);
     assert!(n.is_err());
