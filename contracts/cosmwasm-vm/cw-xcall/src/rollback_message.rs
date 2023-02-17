01af8e342c341de2b2be274b0b266160a94ae995
Prathiksha-Nataraja <prathikshamn28@gmail.com>
2023-02-17 16:54:25 +0530

    format code

--- contracts/cosmwasm-vm/cw-xcall/src/rollback_message.rs
+++ contracts/cosmwasm-vm/cw-xcall/src/rollback_message.rs
@@ -1,69 +1,75 @@
-
-use serde::{Serialize,Deserialize};
+use cosmwasm_std::{from_binary, Event};
+use cosmwasm_std::{
+    DepsMut, Env, IbcChannel, IbcPacketAckMsg, IbcPacketReceiveMsg, IbcPacketTimeoutMsg,
+    IbcReceiveResponse,
+};
 use schemars::JsonSchema;
-use cosmwasm_std::{Event, from_binary};
-use cosmwasm_std::{DepsMut,Env,IbcPacketReceiveMsg,IbcReceiveResponse,IbcChannel,IbcPacketTimeoutMsg,IbcPacketAckMsg};
+use serde::{Deserialize, Serialize};
 
-use crate::msg::{IbcExecuteMsg, self};
+use crate::msg::{self, IbcExecuteMsg};
 use crate::types::address::Address;
-use crate::types::request::{CallServiceMessageRequest, CSMessageRequests, self};
+use crate::types::request::{self, CSMessageRequests, CallServiceMessageRequest};
 use crate::types::response::CSMessageResponse;
-use crate::{ContractError };
+use crate::ContractError;
 
- #[derive(Serialize, Deserialize,Clone, Debug, PartialEq, Eq, JsonSchema)]
- pub struct RollbackMessage{
-    sn : i128,
-    rollback : Vec<u8>,
-    message : String,
- }
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct RollbackMessage {
+    sn: i128,
+    rollback: Vec<u8>,
+    message: String,
+}
 
- impl RollbackMessage {
-   
-    fn rollbackexecuted(&self) -> Event{
-      Event::new("rollbackexecuted").add_attribute("sn", self.sn.to_string())
-                                .add_attribute("rollback", String::from_utf8(self.rollback.clone()).unwrap())
-                                .add_attribute("message", self.message.clone())
+impl RollbackMessage {
+    fn rollbackexecuted(&self) -> Event {
+        Event::new("rollbackexecuted")
+            .add_attribute("sn", self.sn.to_string())
+            .add_attribute(
+                "rollback",
+                String::from_utf8(self.rollback.clone()).unwrap(),
+            )
+            .add_attribute("message", self.message.clone())
     }
- 
 
-   fn rollbackmessage(&self) -> Event{
-      Event::new("rollbackmessage").add_attribute("sn", self.sn.to_string())
+    fn rollbackmessage(&self) -> Event {
+        Event::new("rollbackmessage").add_attribute("sn", self.sn.to_string())
     }
- }
-
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum ExecuteMsg{
-   ExecuteRollBack{sn : i128}
+pub enum ExecuteMsg {
+    ExecuteRollBack { sn: i128 },
 }
 
 fn do_ibc_packet_receive(
-   deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     msg: IbcPacketReceiveMsg,
-)-> Result<IbcReceiveResponse, ContractError>{
-   let msg: IbcExecuteMsg = from_binary(&msg.packet.data)?;
+) -> Result<IbcReceiveResponse, ContractError> {
+    let msg: IbcExecuteMsg = from_binary(&msg.packet.data)?;
 
     match msg {
-        IbcExecuteMsg::Event{sn,rollback,message} => rollbackexecuted(deps,sn,rollback,message),
-      //   IbcExecuteMsg::Event { sn, rollback, message } => execute_rollback(deps,sn),
+        IbcExecuteMsg::Event {
+            sn,
+            rollback,
+            message,
+        } => rollbackexecuted(deps, sn, rollback, message),
     }
-   }
-
-
-fn rollbackexecuted(deps:DepsMut, sn: i128, rollback: Vec<u8> ,message: CallServiceMessageRequest) -> Result<IbcReceiveResponse, ContractError> {
-let r  = try_rollbackexecuted(message );
-Ok(IbcReceiveResponse::new()
-.add_attribute("method","execute_rollbackexecuted"))
 }
 
-fn try_rollbackexecuted(message : CallServiceMessageRequest) -> Result<Vec<u8>,ContractError>{
-   match message.rollback().is_empty(){
-    true => Err(ContractError::Unauthorized { }),
-    false => {
-        return Ok(message.rollback().to_vec())
-    },
+fn rollbackexecuted(
+    deps: DepsMut,
+    sn: i128,
+    rollback: Vec<u8>,
+    message: CallServiceMessageRequest,
+) -> Result<IbcReceiveResponse, ContractError> {
+    let r = try_rollbackexecuted(message);
+    Ok(IbcReceiveResponse::new().add_attribute("method", "execute_rollbackexecuted"))
 }
-   
+
+fn try_rollbackexecuted(message: CallServiceMessageRequest) -> Result<Vec<u8>, ContractError> {
+    match message.rollback().is_empty() {
+        true => Err(ContractError::Unauthorized {}),
+        false => return Ok(message.rollback().to_vec()),
+    }
 }
