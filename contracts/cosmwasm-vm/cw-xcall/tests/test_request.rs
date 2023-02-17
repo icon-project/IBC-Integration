232180785568366484464de2a9a576bf27d72ba9
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-02-17 15:43:18 +0530

    chore: add unit test for request

--- contracts/cosmwasm-vm/cw-xcall/tests/test_request.rs
+++ contracts/cosmwasm-vm/cw-xcall/tests/test_request.rs
@@ -84,3 +84,67 @@ fn update_request_id_without_proper_initialisation() {
 
     assert_eq!(result, 1);
 }
+
+#[test]
+fn set_sequence() {
+    let mut mock_deps = deps();
+
+    let contract = CwCallservice::default();
+
+    contract
+        .last_sequence_no()
+        .save(mock_deps.as_mut().storage, &0)
+        .unwrap();
+
+    contract
+        .set_last_sequence_no(mock_deps.as_mut(), 20)
+        .unwrap();
+
+    let result = contract.query_last_sequence_no(mock_deps.as_ref()).unwrap();
+
+    assert_eq!(result, 20);
+}
+
+#[test]
+fn set_request_id() {
+    let mut mock_deps = deps();
+
+    let contract = CwCallservice::default();
+
+    contract
+        .last_request_id()
+        .save(mock_deps.as_mut().storage, &0)
+        .unwrap();
+
+    contract
+        .set_last_request_id(mock_deps.as_mut(), 20)
+        .unwrap();
+
+    let result = contract.query_last_request_id(mock_deps.as_ref()).unwrap();
+
+    assert_eq!(result, 20);
+}
+
+#[test]
+#[should_panic(expected = "Std(NotFound { kind: \"u128\" })")]
+fn set_sequence_without_proper_initialisation() {
+    let mut mock_deps = deps();
+
+    let contract = CwCallservice::default();
+
+    contract
+        .set_last_sequence_no(mock_deps.as_mut(), 20)
+        .unwrap();
+}
+
+#[test]
+#[should_panic(expected = "Std(NotFound { kind: \"u128\" })")]
+fn set_request_id_without_proper_initialisation() {
+    let mut mock_deps = deps();
+
+    let contract = CwCallservice::default();
+
+    contract
+        .set_last_request_id(mock_deps.as_mut(), 20)
+        .unwrap();
+}
