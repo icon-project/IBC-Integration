3867192d7aacdf62ff0fd33d3c37cf2fa752b00c
AntonAndell <anton@andell.eu>
2023-02-21 06:33:41 +0100

    Add inital unittest, ignoring commitments

--- contracts/javascore/ibc/src/main/java/ibc/ics24/host/IBCStore.java
+++ contracts/javascore/ibc/src/main/java/ibc/ics24/host/IBCStore.java
@@ -13,36 +13,36 @@ import score.VarDB;
 
 public class IBCStore {
         // Commitments
-        public static final DictDB<byte[], byte[]> commitments = Context.newDictDB("commitments", byte[].class);
+        public final DictDB<byte[], byte[]> commitments = Context.newDictDB("commitments", byte[].class);
 
         // Store
-        public static final DictDB<String, Address> clientRegistry = Context.newDictDB("clientRegistry", Address.class);
-        public static final DictDB<String, String> clientTypes = Context.newDictDB("clientTypes", String.class);
-        public static final DictDB<String, Address> clientImpls = Context.newDictDB("clientImpls", Address.class);
-        public static final DictDB<String, ConnectionEnd> connections = Context.newDictDB("connections",
+        public final DictDB<String, Address> clientRegistry = Context.newDictDB("clientRegistry", Address.class);
+        public final DictDB<String, String> clientTypes = Context.newDictDB("clientTypes", String.class);
+        public final DictDB<String, Address> clientImpls = Context.newDictDB("clientImpls", Address.class);
+        public final DictDB<String, ConnectionEnd> connections = Context.newDictDB("connections",
                         ConnectionEnd.class);
-        public static final BranchDB<String, DictDB<String, Channel>> channels = Context.newBranchDB("channels",
+        public final BranchDB<String, DictDB<String, Channel>> channels = Context.newBranchDB("channels",
                         Channel.class);
-        public static final BranchDB<String, DictDB<String, BigInteger>> nextSequenceSends = Context.newBranchDB(
+        public final BranchDB<String, DictDB<String, BigInteger>> nextSequenceSends = Context.newBranchDB(
                         "nextSequenceSends", BigInteger.class);
-        public static final BranchDB<String, DictDB<String, BigInteger>> nextSequenceRecvs = Context.newBranchDB(
+        public final BranchDB<String, DictDB<String, BigInteger>> nextSequenceRecvs = Context.newBranchDB(
                         "nextSequenceRecvs", Address.class);
-        public static final BranchDB<String, DictDB<String, BigInteger>> nextSequenceAcks = Context.newBranchDB(
+        public final BranchDB<String, DictDB<String, BigInteger>> nextSequenceAcks = Context.newBranchDB(
                         "nextSequenceAcks", Address.class);
-        public static final BranchDB<String, BranchDB<String, DictDB<BigInteger, BigInteger>>> packetReceipts = Context
+        public final BranchDB<String, BranchDB<String, DictDB<BigInteger, BigInteger>>> packetReceipts = Context
                         .newBranchDB("packetReceipts", Address.class);
-        public static final BranchDB<byte[], ArrayDB<Address>> capabilities = Context.newBranchDB("capabilities",
+        public final BranchDB<byte[], ArrayDB<Address>> capabilities = Context.newBranchDB("capabilities",
                         Address[].class);
 
         // Host parameters
-        public static final VarDB<BigInteger> expectedTimePerBlock = Context.newVarDB("expectedTimePerBlock",
+        public final VarDB<BigInteger> expectedTimePerBlock = Context.newVarDB("expectedTimePerBlock",
                         BigInteger.class);
 
         // Sequences for identifier
-        public static final VarDB<BigInteger> nextClientSequence = Context.newVarDB("nextClientSequence",
+        public final VarDB<BigInteger> nextClientSequence = Context.newVarDB("nextClientSequence",
                         BigInteger.class);
-        public static final VarDB<BigInteger> nextConnectionSequence = Context.newVarDB("nextConnectionSequence",
+        public final VarDB<BigInteger> nextConnectionSequence = Context.newVarDB("nextConnectionSequence",
                         BigInteger.class);
-        public static final VarDB<BigInteger> nextChannelSequence = Context.newVarDB("nextChannelSequence",
+        public final VarDB<BigInteger> nextChannelSequence = Context.newVarDB("nextChannelSequence",
                         BigInteger.class);
 }
