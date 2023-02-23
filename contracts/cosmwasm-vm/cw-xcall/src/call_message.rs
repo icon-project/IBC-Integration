7b833e9a1ad2f7ac05c25886ca7279e3146a0a80
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-02-23 12:51:25 +0530

    update send packet

--- contracts/cosmwasm-vm/cw-xcall/src/call_message.rs
+++ contracts/cosmwasm-vm/cw-xcall/src/call_message.rs
@@ -1,14 +1,14 @@
-use cosmwasm_std::{
-    ensure, to_binary, Addr, Binary, Deps, DepsMut, Env, Event, IbcMsg, MessageInfo, Response,
-};
+use cosmwasm_std::{to_binary, Binary, DepsMut, Env, Event, IbcMsg, MessageInfo, Response};
 use cosmwasm_std::{IbcTimeout, IbcTimeoutBlock};
 
 use crate::error::ContractError;
 use crate::state::CwCallservice;
-use crate::types::address::Address;
-use crate::types::message::CallServiceMessage;
-use crate::types::message::CallServiceMessageType;
-use crate::types::request::CallServiceMessageRequest;
+use crate::types::{
+    address::Address,
+    call_request::CallRequest,
+    message::{CallServiceMessage, CallServiceMessageType},
+    request::CallServiceMessageRequest,
+};
 
 impl<'a> CwCallservice<'a> {
     pub fn send_packet(
@@ -21,6 +21,7 @@ impl<'a> CwCallservice<'a> {
         rollback: Binary,
         time_out_height: u64,
     ) -> Result<Response, ContractError> {
+        let from_address = info.sender.to_string();
         self.ensure_caller_is_contract_and_rollback_is_null(
             deps.as_ref(),
             info.sender.clone(),
@@ -31,15 +32,40 @@ impl<'a> CwCallservice<'a> {
 
         self.ensure_rollback_length(&rollback.0)?;
 
-        let (packet, event) = self.create_packet_and_event_for_request(
-            deps,
-            env,
-            info.clone(),
+        // TODO : ADD fee logic
+
+        let need_response = rollback.is_empty();
+
+        let sequence_no = self.increment_last_sequence_no(deps.storage)?;
+
+        if need_response {
+            let request = CallRequest::new(
+                Address::from(&from_address),
+                to.clone(),
+                rollback.clone(),
+                need_response,
+            );
+
+            self.set_call_request(deps.storage, sequence_no, request)?;
+        }
+
+        let call_request = CallServiceMessageRequest::new(
+            Address::from(info.sender.as_str()),
             to,
+            sequence_no,
             rollback,
-            time_out_height,
-            data.clone(),
-        )?;
+            data,
+        );
+
+        let message = CallServiceMessage::new(
+            CallServiceMessageType::CallServiceRequest,
+            to_binary(&call_request).unwrap(),
+        );
+
+        let packet =
+            self.create_packet_and_event_for_request(deps, env, time_out_height, message.clone())?;
+
+        let event = event_xcall_message_sent(sequence_no, info.sender.to_string(), 0, &message);
 
         Ok(Response::new()
             .add_message(packet)
@@ -52,19 +78,14 @@ impl<'a> CwCallservice<'a> {
         &self,
         deps: DepsMut,
         env: Env,
-
-        info: MessageInfo,
-        to: String,
-        rollback: Binary,
         time_out_height: u64,
-        data: Binary,
-    ) -> Result<(IbcMsg, Event), ContractError> {
+        message: CallServiceMessage,
+    ) -> Result<IbcMsg, ContractError> {
         let ibc_config = self
             .ibc_config()
             .may_load(deps.as_ref().storage)
             .unwrap()
             .unwrap();
-        let seq_no = self.increment_last_sequence_no(deps).unwrap();
 
         let timeout_block = IbcTimeoutBlock {
             revision: 0,
@@ -72,29 +93,11 @@ impl<'a> CwCallservice<'a> {
         };
         let timeout = IbcTimeout::with_both(timeout_block, env.block.time.plus_seconds(300));
 
-        let call_request = CallServiceMessageRequest::new(
-            Address::from(info.sender.as_str()),
-            to,
-            seq_no,
-            rollback,
-            data,
-        );
-
-        let message = CallServiceMessage::new(
-            CallServiceMessageType::CallServiceRequest,
-            to_binary(&call_request).unwrap(),
-        );
-
-        let event = event_xcall_message_sent(seq_no, info.sender.to_string(), 0, &message);
-
-        Ok((
-            IbcMsg::SendPacket {
-                channel_id: ibc_config.dst_endpoint().channel_id.clone(),
-                data: to_binary(&message).unwrap(),
-                timeout,
-            },
-            event,
-        ))
+        Ok(IbcMsg::SendPacket {
+            channel_id: ibc_config.dst_endpoint().channel_id.clone(),
+            data: to_binary(&message).unwrap(),
+            timeout,
+        })
     }
 }
 
@@ -112,38 +115,3 @@ fn event_xcall_message_sent(
         .add_attribute("req_id", req_id.to_string())
         .add_attribute("data", to_binary(data).unwrap().to_string())
 }
-
-// #[test]
-// fn test() {
-//     let mut mock_deps = mock_dependencies();
-
-//     let contrat = CwCallservice::default();
-//     let env = mock_env();
-
-//     let src = IbcEndpoint {
-//         port_id: "our-port".to_string(),
-//         channel_id: "channel-1".to_string(),
-//     };
-
-//     let dst = IbcEndpoint {
-//         port_id: "their-port".to_string(),
-//         channel_id: "channel-3".to_string(),
-//     };
-
-//     let ibc_config = IbcConfig::new(src, dst);
-
-//     contrat
-//         .ibc_config()
-//         .save(mock_deps.as_mut().storage, &ibc_config)
-//         .unwrap();
-
-//     let ibc_msg = contrat
-//         .send_call_message(env.clone(), mock_deps.as_ref(), 10, &vec![1, 2, 3])
-//         .unwrap();
-
-//     println!("{:?}", ibc_msg);
-
-//     let msgs = mock_ibc_packet_recv("channel-1", &vec![1, 2, 3]).unwrap();
-
-//     let r = ibc_packet_receive(mock_deps.as_mut(), env, msgs);
-// }
