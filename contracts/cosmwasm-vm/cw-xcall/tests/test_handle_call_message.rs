ca36a41f069b436113392776864d81a112fd04a3
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-02-24 14:25:04 +0530

    chore: fix test and add methoda to call message

--- contracts/cosmwasm-vm/cw-xcall/tests/test_handle_call_message.rs
+++ contracts/cosmwasm-vm/cw-xcall/tests/test_handle_call_message.rs
@@ -1,16 +1,22 @@
 use cosmwasm_std::{
-    coin,
+    from_binary,
     testing::{mock_dependencies, mock_env, mock_info},
-    to_binary, Binary, Coin, CosmosMsg, Event, Reply, SubMsgResponse, SubMsgResult, WasmMsg,
+    Binary, Coin, CosmosMsg, IbcEndpoint, IbcMsg, Reply, SubMsgResponse, SubMsgResult, WasmMsg,
 };
 use cw_xcall::{
-    error::ContractError,
-    handle_callmessage::reply,
-    state::CwCallservice,
-    types::{address::Address, request::CallServiceMessageRequest},
+    state::{CwCallservice, IbcConfig, EXECUTE_CALL},
+    types::{
+        address::Address,
+        message::{CallServiceMessage, CallServiceMessageType},
+        request::CallServiceMessageRequest,
+        response::{CallServiceMessageReponse, CallServiceResponseType},
+    },
 };
+mod account;
+mod setup;
 
-const EXECUTE_CALL: u64 = 0;
+use setup::*;
+
 #[test]
 #[should_panic(expected = "InvalidRequestId")]
 fn test_execute_call_invalid_request_id() {
@@ -68,43 +74,152 @@ fn test_execute_call_having_request_id_without_rollback() {
 
 #[test]
 fn test_successful_reply_message() {
-    let event = Event::new("callexecuted")
-        .add_attribute("request_id", 2.to_string())
-        .add_attribute("code", "code".to_string())
-        .add_attribute("msg", "msg".to_string());
+    let mut mock_deps = deps();
+
+    let env = mock_env();
+
     let msg = Reply {
         id: EXECUTE_CALL,
         result: SubMsgResult::Ok(SubMsgResponse {
-            events: vec![event],
+            events: vec![],
             data: None,
         }),
     };
-    let env = mock_env();
-    let mut deps = mock_dependencies();
-    let res = reply(deps.as_mut(), env, msg).unwrap();
 
-    assert_eq!(1, res.attributes.len());
-    assert_eq!("call_message", res.attributes[0].key);
-    assert_eq!(1, res.events.len());
+    let contract = CwCallservice::default();
+
+    let request_id = 123456;
+    let proxy_reqs = CallServiceMessageRequest::new(
+        Address::from(" 88bd05442686be0a5df7da33b6f1089ebfea3769b19dbb2477fe0cd6e0f126e4"),
+        "88bd05442686be0a5df7da33b6f1089ebfea3769b19dbb2477fe0cd6e0f123t7".to_owned(),
+        123,
+        Binary::from(vec![]),
+        Binary::from(vec![]),
+    );
+    contract
+        .insert_request(mock_deps.as_mut().storage, request_id, proxy_reqs)
+        .unwrap();
+
+    contract
+        .last_request_id()
+        .save(mock_deps.as_mut().storage, &request_id)
+        .unwrap();
+
+    let response = contract.reply(mock_deps.as_mut(), env, msg).unwrap();
+
+    assert_eq!(response.events[0].attributes[1].value, 0.to_string());
 }
 
 #[test]
 fn test_failed_reply_message() {
+    let mut mock_deps = deps();
+
+    let env = mock_env();
+
     let msg = Reply {
         id: EXECUTE_CALL,
         result: SubMsgResult::Err("error message".into()),
     };
-    let env = mock_env();
-    let mut deps = mock_dependencies();
-    let res = reply(deps.as_mut(), env, msg);
 
-    assert!(res.is_err());
-    assert_eq!(
-        ContractError::ReplyError {
-            code: 1,
-            msg: "error message".into()
-        }
-        .to_string(),
-        res.unwrap_err().to_string()
+    let contract = CwCallservice::default();
+
+    let request_id = 123456;
+    let proxy_reqs = CallServiceMessageRequest::new(
+        Address::from(" 88bd05442686be0a5df7da33b6f1089ebfea3769b19dbb2477fe0cd6e0f126e4"),
+        "88bd05442686be0a5df7da33b6f1089ebfea3769b19dbb2477fe0cd6e0f123t7".to_owned(),
+        123,
+        Binary::from(vec![]),
+        Binary::from(vec![]),
     );
+    contract
+        .insert_request(mock_deps.as_mut().storage, request_id, proxy_reqs)
+        .unwrap();
+
+    contract
+        .last_request_id()
+        .save(mock_deps.as_mut().storage, &request_id)
+        .unwrap();
+
+    let response = contract.reply(mock_deps.as_mut(), env, msg).unwrap();
+
+    assert_eq!(response.events[0].attributes[1].value, "-1".to_string());
+}
+
+#[test]
+fn check_for_rollback_in_response() {
+    let mut mock_deps = deps();
+
+    let env = mock_env();
+
+    let msg = Reply {
+        id: EXECUTE_CALL,
+        result: SubMsgResult::Ok(SubMsgResponse {
+            events: vec![],
+            data: None,
+        }),
+    };
+
+    let contract = CwCallservice::default();
+
+    let request_id = 123456;
+    let proxy_reqs = CallServiceMessageRequest::new(
+        Address::from(" 88bd05442686be0a5df7da33b6f1089ebfea3769b19dbb2477fe0cd6e0f126e4"),
+        "88bd05442686be0a5df7da33b6f1089ebfea3769b19dbb2477fe0cd6e0f123t7".to_owned(),
+        123,
+        Binary::from(vec![1, 2, 3]),
+        Binary::from(vec![]),
+    );
+    contract
+        .insert_request(mock_deps.as_mut().storage, request_id, proxy_reqs)
+        .unwrap();
+
+    contract
+        .last_request_id()
+        .save(mock_deps.as_mut().storage, &request_id)
+        .unwrap();
+
+    let src = IbcEndpoint {
+        port_id: "our-port".to_string(),
+        channel_id: "channel-1".to_string(),
+    };
+
+    let dst = IbcEndpoint {
+        port_id: "their-port".to_string(),
+        channel_id: "channel-3".to_string(),
+    };
+
+    let ibc_config = IbcConfig::new(src, dst);
+
+    contract
+        .ibc_config()
+        .save(mock_deps.as_mut().storage, &ibc_config)
+        .unwrap();
+
+    let response = contract.reply(mock_deps.as_mut(), env, msg).unwrap();
+
+    // let result = CosmosMsg::Ibc(cosmwasm_std::IbcMsg::SendPacket { channel_id: (), data: (), timeout: () });
+
+    match response.messages[0].msg.clone() {
+        CosmosMsg::Ibc(IbcMsg::SendPacket {
+            channel_id: _,
+            data,
+            timeout: _,
+        }) => {
+            let result: CallServiceMessage = from_binary(&data).unwrap();
+            assert_eq!(
+                result.message_type().to_owned(),
+                CallServiceMessageType::CallServiceResponse
+            );
+
+            let payload: CallServiceMessageReponse =
+                from_binary(&Binary::from(result.payload().to_vec())).unwrap();
+
+            assert_eq!(
+                payload.response_code().to_owned(),
+                CallServiceResponseType::CallServiceResponseSucess
+            )
+        }
+
+        _ => todo!(),
+    };
 }
