5775b1db77793be3b86c060cdf952068547074f2
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-03-14 14:23:53 +0530

    chore: implement primary key trait for Ibc types

--- contracts/cosmwasm-vm/cw-ibc-core/src/lib.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/lib.rs
@@ -9,15 +9,23 @@ pub mod ics24_host;
 pub mod msg;
 pub mod state;
 pub mod storage_keys;
+pub mod types;
 
 pub use crate::error::ContractError;
-use crate::storage_keys::StorageKey;
+use crate::{
+    storage_keys::StorageKey,
+    types::{ChannelId, ClientId, ClientType, ConnectionId, PortId},
+};
 use cosmwasm_schema::cw_serde;
 use cw_storage_plus::{Item, Map};
 use ibc::core::{
-    ics02_client::client_type::ClientType,
+    ics02_client::client_type::ClientType as IbcClientType,
     ics03_connection::connection::ConnectionEnd,
     ics04_channel::channel::ChannelEnd,
     ics04_channel::packet::Sequence,
-    ics24_host::identifier::{ChannelId, ClientId, ConnectionId, PortId},
+    ics24_host::identifier::{
+        ChannelId as IbcChannelId, ClientId as IbcClientId, ConnectionId as IbcConnectionId,
+        PortId as IbcPortId,
+    },
 };
+use serde::{Deserialize, Serialize};
---
+++ contracts/cosmwasm-vm/cw-ibc-core/src/types.rs
@@ -0,0 +1,97 @@
+use std::str::FromStr;
+
+use cosmwasm_std::StdError;
+use cw_storage_plus::{Key, KeyDeserialize, PrimaryKey};
+
+use super::*;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ClientId(IbcClientId);
+
+impl<'a> PrimaryKey<'a> for ClientId {
+    type Prefix = ();
+
+    type SubPrefix = ();
+
+    type Suffix = Self;
+
+    type SuperSuffix = Self;
+
+    fn key(&self) -> Vec<Key> {
+        vec![Key::Ref(self.0.as_bytes())]
+    }
+}
+
+impl KeyDeserialize for ClientId {
+    type Output = ClientId;
+
+    fn from_vec(value: Vec<u8>) -> cosmwasm_std::StdResult<Self::Output> {
+        let result = String::from_utf8(value)
+            .map_err(StdError::invalid_utf8)
+            .unwrap();
+        let client_id = IbcClientId::from_str(&result).unwrap();
+        Ok(ClientId(client_id))
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ClientType(IbcClientType);
+
+impl<'a> PrimaryKey<'a> for ClientType {
+    type Prefix = ();
+
+    type SubPrefix = ();
+
+    type Suffix = Self;
+
+    type SuperSuffix = Self;
+
+    fn key(&self) -> Vec<Key> {
+        vec![Key::Ref(self.0.as_str().as_bytes())]
+    }
+}
+
+impl KeyDeserialize for ClientType {
+    type Output = ClientType;
+
+    fn from_vec(value: Vec<u8>) -> cosmwasm_std::StdResult<Self::Output> {
+        let result = String::from_utf8(value)
+            .map_err(StdError::invalid_utf8)
+            .unwrap();
+        let client_type = IbcClientType::new(result);
+        Ok(ClientType(client_type))
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ChannelId(IbcChannelId);
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct ConnectionId(IbcConnectionId);
+
+impl<'a> PrimaryKey<'a> for ConnectionId {
+    type Prefix = ();
+
+    type SubPrefix = ();
+
+    type Suffix = Self;
+
+    type SuperSuffix = Self;
+    fn key(&self) -> Vec<Key> {
+        vec![Key::Ref(self.0.as_str().as_bytes())]
+    }
+}
+impl KeyDeserialize for ConnectionId {
+    type Output = ConnectionId;
+
+    fn from_vec(value: Vec<u8>) -> cosmwasm_std::StdResult<Self::Output> {
+        let result = String::from_utf8(value)
+            .map_err(StdError::invalid_utf8)
+            .unwrap();
+        let connection_id = IbcConnectionId::from_str(&result).unwrap();
+        Ok(ConnectionId(connection_id))
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PortId(IbcPortId);
