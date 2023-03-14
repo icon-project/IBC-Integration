465aed3cd07b9b9f557ce1581479402dae4cb1e9
SHANiTH K K <59169878+shanithkk@users.noreply.github.com>
2023-03-14 11:04:12 +0530

    feat: implement storage for channel (#80)
    * chore: implement Storage for Ibc core
    
    * chore: define storage for channel
    
    * chore: implement storage keys for Ibc store
    
    ---------
    
    Co-authored-by: shreyasbhat0 <shreyas.ks@hugobyte.com>

--- contracts/cosmwasm-vm/cw-ibc-core/src/lib.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/lib.rs
@@ -8,5 +8,15 @@ pub mod ics05_port;
 pub mod ics24_host;
 pub mod msg;
 pub mod state;
+pub mod storage_keys;
 
 pub use crate::error::ContractError;
+use crate::storage_keys::StorageKey;
+use cosmwasm_schema::cw_serde;
+use cw_storage_plus::{Item, Map};
+use ibc::core::{
+    ics02_client::client_type::ClientType,
+    ics04_channel::packet::Sequence,
+    ics24_host::identifier::{ChannelId, ClientId, PortId},
+    ics04_channel::channel::ChannelEnd,
+};
