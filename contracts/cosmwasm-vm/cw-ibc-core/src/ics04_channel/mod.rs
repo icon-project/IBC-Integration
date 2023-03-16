6c7236db3eafb96f1b3938f00a46550c88c5bd57
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-03-16 18:13:11 +0530

    chore: remove unused import

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics04_channel/mod.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics04_channel/mod.rs
@@ -1,7 +1,5 @@
 //! ICS 04: Channel implementation that facilitates communication between
 pub mod channel;
-pub use channel::*;
-
-use self::state::CwIbcStore;
 pub use super::*;
+pub use channel::*;
 use cosmwasm_std::Storage;
