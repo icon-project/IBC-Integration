9800cf96f3615e17e2729176fbc00e28a5d53d2c
Shanith K K <shanith.kk@hugobyte.com>
2023-03-20 20:06:55 +0530

    chore: Change ibcevent to cosmos event

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics04_channel/events.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics04_channel/events.rs
@@ -1,58 +1,48 @@
 use super::*;
 
 // Makes OpenInitChannel IBC Event
-pub fn make_open_init_channel_event(channel_id: &ChannelId, msg: &MsgChannelOpenInit) -> IbcEvent {
-    let connection_id = msg.connection_hops_on_a[0].clone();
-    let open_init_data = OpenInit::new(
-        msg.port_id_on_a.clone(),
-        channel_id.ibc_channel_id().clone(),
-        msg.port_id_on_b.clone(),
-        connection_id,
-        msg.version_proposal.clone(),
-    );
-
-    IbcEvent::OpenInitChannel(open_init_data)
+pub fn make_open_init_channel_event(channel_id: &ChannelId, msg: &MsgChannelOpenInit) -> Event {
+    Event::new(IbcEventType::OpenInitChannel.as_str())
+        .add_attribute("port_id", msg.port_id_on_a.as_str())
+        .add_attribute("channel_id", channel_id.as_str())
+        .add_attribute("counterparty_port_id", msg.port_id_on_b.as_str())
+        .add_attribute("connection_id", msg.connection_hops_on_a[0].as_str())
+        .add_attribute("version", msg.version_proposal.as_str())
 }
 
 // Makes OpenInitChannel IBC Event
-pub fn make_open_try_channel_event(channel_id: &ChannelId, msg: &MsgChannelOpenTry) -> IbcEvent {
-    let open_try_data = OpenTry::new(
-        msg.port_id_on_b.clone(),
-        channel_id.ibc_channel_id().clone(),
-        msg.port_id_on_a.clone(),
-        msg.chan_id_on_a.clone(),
-        msg.connection_hops_on_b[0].clone(),
-        msg.version_supported_on_a.clone(),
-    );
-    IbcEvent::OpenTryChannel(open_try_data)
+pub fn make_open_try_channel_event(channel_id: &ChannelId, msg: &MsgChannelOpenTry) -> Event {
+    Event::new(IbcEventType::OpenTryChannel.as_str())
+        .add_attribute("port_id", msg.port_id_on_b.as_str())
+        .add_attribute("channel_id", channel_id.as_str())
+        .add_attribute("counterparty_port_id", msg.port_id_on_a.as_str())
+        .add_attribute("counterparty_channel_id", msg.chan_id_on_a.as_str())
+        .add_attribute("connection_id", msg.connection_hops_on_b[0].as_str())
+        .add_attribute("version", msg.version_supported_on_a.as_str())
 }
 
 // Makes OpenAckChannel  IBC Event
-pub fn make_open_ack_channel_event(msg: &MsgChannelOpenAck) -> IbcEvent {
-    let open_try_data = OpenAck::new(
-        msg.port_id_on_a.clone(),
-        msg.chan_id_on_a.clone(),
-        PortId::dafault().ibc_port_id().clone(),
-        msg.chan_id_on_b.clone(),
-        ConnectionId::default().connection_id().clone(),
-    );
-    IbcEvent::OpenAckChannel(open_try_data)
+pub fn make_open_ack_channel_event(msg: &MsgChannelOpenAck) -> Event {
+    Event::new(IbcEventType::OpenAckChannel.as_str())
+        .add_attribute("port_id", msg.port_id_on_a.as_str())
+        .add_attribute("channel_id", msg.chan_id_on_a.as_str())
+        .add_attribute("counterparty_port_id", "")
+        .add_attribute("counterparty_channel_id", msg.chan_id_on_b.as_str())
+        .add_attribute("connection_id", "")
 }
 
 // Makes OpenConfirmChannel IBC Event
-pub fn make_open_confirm_channel_event(msg: &MsgChannelOpenConfirm) -> IbcEvent {
-    let open_try_data = OpenConfirm::new(
-        msg.port_id_on_b.clone(),
-        msg.chan_id_on_b.clone(),
-        PortId::dafault().ibc_port_id().clone(),
-        ChannelId::default().ibc_channel_id().clone(),
-        ConnectionId::default().connection_id().clone(),
-    );
-    IbcEvent::OpenConfirmChannel(open_try_data)
+pub fn make_open_confirm_channel_event(msg: &MsgChannelOpenConfirm) -> Event {
+    Event::new(IbcEventType::OpenAckChannel.as_str())
+        .add_attribute("port_id", msg.port_id_on_b.as_str())
+        .add_attribute("channel_id", msg.chan_id_on_b.as_str())
+        .add_attribute("counterparty_port_id", "")
+        .add_attribute("counterparty_channel_id", "")
+        .add_attribute("connection_id", "")
 }
 
 // Event for created channel id
-pub fn event_channel_id_create(channel_id: ChannelId) -> CosmosEvent {
-    CosmosEvent::new("ChannelId Created")
+pub fn event_channel_id_create(channel_id: ChannelId) -> Event {
+    Event::new("ChannelId Created")
         .add_attribute("channel_id", channel_id.ibc_channel_id().as_str())
 }
--- contracts/cosmwasm-vm/cw-ibc-core/src/ics04_channel/mod.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics04_channel/mod.rs
@@ -7,7 +7,7 @@ pub use events::*;
 pub use super::*;
 use crate::context::CwIbcCoreContext;
 pub use channel::*;
-use cosmwasm_std::Event as CosmosEvent;
+use cosmwasm_std::Event;
 use cosmwasm_std::Storage;
 pub use ibc::core::ics04_channel::events::*;
 pub use ibc::core::ics04_channel::msgs::{
@@ -15,5 +15,5 @@ pub use ibc::core::ics04_channel::msgs::{
     chan_open_ack::MsgChannelOpenAck, chan_open_confirm::MsgChannelOpenConfirm,
     chan_open_init::MsgChannelOpenInit, chan_open_try::MsgChannelOpenTry, ChannelMsg,
 };
-use ibc::events::IbcEvent;
+use ibc::events::IbcEventType;
 use ibc_proto::protobuf::Protobuf;
