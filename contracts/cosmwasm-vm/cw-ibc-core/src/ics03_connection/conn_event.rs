5710d3079985c2d38d4cff16f293153efad9c502
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-03-20 11:54:49 +0530

    chore: added event openinit

---
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/conn_event.rs
@@ -0,0 +1,10 @@
+use super::*;
+pub fn event_open_init(conn_id: ConnectionId, msg: MsgConnectionOpenInit) -> IbcEvent {
+    let open_init = OpenInit::new(
+        conn_id.connection_id().clone(),
+        msg.client_id_on_a.clone(),
+        msg.counterparty.client_id().clone(),
+    );
+    IbcEvent::OpenInitConnection(open_init)
+}
+
--- contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/mod.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/mod.rs
@@ -1,4 +1,5 @@
 //! ICS 03: Connection implementation for connecting a client
+pub mod conn_event;
 pub mod connection;
 use crate::context::CwIbcCoreContext;
 use crate::types::{ClientId, ConnectionId};
@@ -6,4 +7,8 @@ use crate::ContractError;
 use cosmwasm_std::Storage;
 use ibc::core::ics03_connection::connection::ConnectionEnd;
 use ibc::core::ics23_commitment::commitment::CommitmentPrefix;
+use ibc::{
+    core::ics03_connection::{events::OpenInit, msgs::conn_open_init::MsgConnectionOpenInit},
+    events::IbcEvent,
+};
 use ibc_proto::protobuf::Protobuf;
