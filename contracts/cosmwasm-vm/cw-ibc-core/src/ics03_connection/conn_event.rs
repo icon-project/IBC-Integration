25102faf5ebfdcd0734d8599dc7d66968dc7abd3
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-03-21 10:40:26 +0530

    chore: updated mod and conn_event

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/conn_event.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/conn_event.rs
@@ -1,9 +1,15 @@
 use super::*;
-pub fn event_open_init(conn_id: ConnectionId, msg: MsgConnectionOpenInit) -> IbcEvent {
-    let open_init = OpenInit::new(
-        conn_id.connection_id().clone(),
-        msg.client_id_on_a.clone(),
-        msg.counterparty.client_id().clone(),
-    );
-    IbcEvent::OpenInitConnection(open_init)
+
+pub fn event_open_init(
+    connection_id: ConnectionId,
+    client_id: ClientId,
+    counterparty_client_id: ClientId,
+) -> Event {
+    Event::new(IbcEventType::OpenInitConnection.as_str())
+        .add_attribute(CONN_ID_ATTRIBUTE_KEY, connection_id.as_str())
+        .add_attribute(CLIENT_ID_ATTRIBUTE_KEY, client_id.as_str())
+        .add_attribute(
+            COUNTERPARTY_CLIENT_ID_ATTRIBUTE_KEY,
+            counterparty_client_id.as_str(),
+        )
 }
