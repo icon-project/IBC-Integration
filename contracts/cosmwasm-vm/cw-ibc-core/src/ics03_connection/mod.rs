b0178d844ba46dc97670674055569c7c8fe5abf8
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-03-21 13:05:32 +0530

    feat: implement messages for openinit (#119)
    * chore: added event openinit
    
    * chore: updated connopeninit event
    
    * chore: updated mod and conn_event
    
    ---------
    
    Co-authored-by: shreyasbhat0 <shreyas.ks@hugobyte.com>

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/mod.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/mod.rs
@@ -1,9 +1,17 @@
 //! ICS 03: Connection implementation for connecting a client
+pub mod conn_event;
 pub mod connection;
 use crate::context::CwIbcCoreContext;
 use crate::types::{ClientId, ConnectionId};
 use crate::ContractError;
+use cosmwasm_std::Event;
 use cosmwasm_std::Storage;
 use ibc::core::ics03_connection::connection::ConnectionEnd;
 use ibc::core::ics23_commitment::commitment::CommitmentPrefix;
+use ibc::{
+    core::ics03_connection::events::{
+        CLIENT_ID_ATTRIBUTE_KEY, CONN_ID_ATTRIBUTE_KEY, COUNTERPARTY_CLIENT_ID_ATTRIBUTE_KEY,
+    },
+    events::IbcEventType,
+};
 use ibc_proto::protobuf::Protobuf;
