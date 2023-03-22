f3e81a47b49934267dc8f0d3cfcc2db07d18f54d
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-03-22 19:01:32 +0530

    chore: add unit test

--- contracts/cosmwasm-vm/cw-ibc-core/tests/test_client.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/tests/test_client.rs
@@ -3,7 +3,8 @@ pub mod setup;
 use cw_ibc_core::{
     context::CwIbcCoreContext,
     ics02_client::events::{
-        client_misbehaviour_event, create_client_event, update_client_event, upgrade_client_event,
+        client_misbehaviour_event, create_client_event, generated_client_id_event,
+        update_client_event, upgrade_client_event,
     },
     types::{ClientId, ClientType},
     MsgCreateClient, MsgUpdateClient, MsgUpgradeClient,
@@ -18,7 +19,6 @@ use ibc::{
 use setup::*;
 
 #[test]
-
 fn get_client_next_sequence() {
     let mut mock = deps();
 
@@ -139,7 +139,7 @@ fn test_create_client_event() {
 }
 
 #[test]
-fn test_raw_update_client_event() {
+fn check_for_update_client_event() {
     let raw_message = get_dummy_raw_msg_update_client_message();
     let message: MsgUpdateClient = MsgUpdateClient::try_from(raw_message.clone()).unwrap();
     let height = Height::new(15, 10).unwrap();
@@ -152,12 +152,17 @@ fn test_raw_update_client_event() {
     );
 
     assert_eq!("update_client", result.ty);
+}
 
+#[test]
+fn check_for_raw_message_to_update_client_message() {
+    let raw_message = get_dummy_raw_msg_update_client_message();
+    let message: MsgUpdateClient = MsgUpdateClient::try_from(raw_message.clone()).unwrap();
     assert_eq!(raw_message, message.into())
 }
 
 #[test]
-fn test_upgrade_client_event() {
+fn check_for_raw_message_to_updgrade_client() {
     let client_type = ClientType::new("new_cleint_type".to_string());
     let client_id = ClientId::new(client_type.clone(), 10).unwrap();
     let signer = get_dummy_account_id();
@@ -184,10 +189,35 @@ fn test_upgrade_client_event() {
     let upgrade_message_from_raw_message = MsgUpgradeClient::try_from(raw_message).unwrap();
 
     assert_eq!(upgrade_message_from_raw_message, msg);
+}
+
+#[test]
+fn test_upgrade_client_event() {
+    let client_type = ClientType::new("new_cleint_type".to_string());
+    let client_id = ClientId::new(client_type.clone(), 10).unwrap();
+    let signer = get_dummy_account_id();
+
+    let height = Height::new(1, 1).unwrap();
+
+    let client_state = MockClientState::new(MockHeader::new(height));
+    let consensus_state = MockConsensusState::new(MockHeader::new(height));
+
+    let proof = get_dummy_merkle_proof();
+
+    let msg = MsgUpgradeClient {
+        client_id: client_id.ibc_client_id().clone(),
+        client_state: client_state.into(),
+        consensus_state: consensus_state.into(),
+        proof_upgrade_client: proof.clone(),
+        proof_upgrade_consensus_state: proof,
+        signer,
+    };
 
     let event = upgrade_client_event(client_type.client_type(), height, msg);
 
-    assert_eq!("upgrade_client", event.ty)
+    assert_eq!("upgrade_client", event.ty);
+
+    assert_eq!(event.attributes[0].value, "new_cleint_type-10")
 }
 
 #[test]
@@ -281,4 +311,18 @@ fn check_for_create_client_message_into_raw_message() {
         ibc_proto::ibc::core::client::v1::MsgCreateClient::try_from(actual_message).unwrap();
 
     assert_eq!(raw_message, get_dummy_raw_msg_create_client())
+}
+
+#[test]
+fn check_for_genereted_client_id_event() {
+    let client_type = ClientType::new("new_cleint_type".to_string());
+    let client_id = ClientId::new(client_type.clone(), 10).unwrap();
+    let event = generated_client_id_event(client_id.ibc_client_id().clone());
+
+    assert_eq!("client_id_created", event.ty);
+
+    assert_eq!(
+        event.attributes[0].value,
+        client_id.ibc_client_id().as_str()
+    )
 }
