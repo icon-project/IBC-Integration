f7aa752e5239bfb0e59e849ef72bd47bab14374c
Shreyas S Bhat <35568964+shreyasbhat0@users.noreply.github.com>
2023-03-24 18:23:41 +0530

    feat: implement events for creating client and misbehaviour (#146)
    * chore: rename client_impls
    
    * chore: cargo fmt
    
    * chore: add commitment and update ibc store
    
    * chore: remove cargo lock

--- contracts/cosmwasm-vm/cw-ibc-core/tests/test_storage.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/tests/test_storage.rs
@@ -1,5 +1,5 @@
 pub mod setup;
-use cosmwasm_std::Addr;
+use cosmwasm_std::{testing::mock_env, Addr};
 use cw_ibc_core::{context::CwIbcCoreContext, types::ModuleId};
 use setup::*;
 
@@ -115,4 +115,21 @@ fn check_for_invalid_route() {
     let result = contract.has_route(deps.as_mut().storage, module_id_new);
 
     assert_eq!(false, result)
+}
+
+#[test]
+fn check_for_setting_valid_block_height() {
+    let mut deps = deps();
+    let contract = CwIbcCoreContext::default();
+
+    let mock_env = mock_env();
+
+    contract
+        .block_height()
+        .save(deps.as_mut().storage, &mock_env.block.height)
+        .unwrap();
+
+    let result = contract.block_height().load(deps.as_ref().storage).unwrap();
+
+    assert_eq!(mock_env.block.height, result);
 }
