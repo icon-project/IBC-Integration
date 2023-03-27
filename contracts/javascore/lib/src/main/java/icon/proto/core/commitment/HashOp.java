6aa2aef06f68a01b4be1927f766891556b34e72a
Night Owl <91905208+nightowl121@users.noreply.github.com>
2023-03-27 17:04:29 +0545

    feat: add proto files for ics 23 vector commitment (#157)
    * feat(proto): add proto files for vector commitment
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    
    * feat: add generated files of proofs.proto for java score
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    
    * build(javascore): marked proto generated java file as linguist-generated
    
    ---------
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    Co-authored-by: red__larva <red.x.larva@protonmail.com>

---
+++ contracts/javascore/lib/src/main/java/icon/proto/core/commitment/HashOp.java
@@ -0,0 +1,17 @@
+package icon.proto.core.commitment;
+
+public class HashOp {
+  public static final int NO_HASH = 0;
+
+  public static final int SHA256 = 1;
+
+  public static final int SHA512 = 2;
+
+  public static final int KECCAK = 3;
+
+  public static final int RIPEMD160 = 4;
+
+  public static final int BITCOIN = 5;
+
+  public static final int SHA512_256 = 6;
+}
