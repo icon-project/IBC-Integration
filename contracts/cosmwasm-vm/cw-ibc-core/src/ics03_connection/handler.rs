51806d6acad9a24fde1d1745029268cbfff213a2
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-03-30 15:55:00 +0530

    chore: removed unused imports

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/handler.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/handler.rs
@@ -1,5 +1,3 @@
-use ibc::core::ics24_host::identifier::ClientId;
-
 use super::*;
 
 impl<'a> CwIbcCoreContext<'a> {
@@ -10,7 +8,7 @@ impl<'a> CwIbcCoreContext<'a> {
         client_id: ClientId,
     ) -> Result<Response, ContractError> {
         // validate
-        self.client_state(&message.client_id_on_a);
+        // self.client_state(&message.client_id_on_a);
         let v = get_compatible_versions();
 
         let versions = match message.version {
@@ -40,7 +38,7 @@ impl<'a> CwIbcCoreContext<'a> {
         let r = message.counterparty.client_id().clone();
         let client_id_on_b = crate::ClientId::from(r);
         let event = create_open_init_event(
-            conn_id,
+            conn_id.clone(),
             crate::ClientId::from(message.client_id_on_a.clone()),
             client_id_on_b,
         );
@@ -48,8 +46,8 @@ impl<'a> CwIbcCoreContext<'a> {
             Ok(counter) => counter,
             Err(error) => return Err(error),
         };
-        self.store_connection_to_client(deps.storage, crate::ClientId::from(client_id), conn_id)?;
-        self.store_connection(deps.storage, conn_id.clone(), conn_end)
+        self.store_connection_to_client(deps.storage, client_id, conn_id.clone())?;
+        self.store_connection(deps.storage, conn_id, conn_end)
             .unwrap();
         return Ok(Response::new().add_event(event));
     }
