e14803672c10672887fa143534d3ce48418c849f
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-04-03 18:00:33 +0530

    feat: 67-user-story-connection-implementing-openinit-handshake (#165)
    * chore: initial commit
    
    * chore: implement register client and client identifier
    
    * chore: added connection openinit handshake
    
    * chore: added unit testcases for openinit
    
    * chore: update connection error
    
    * chore: update imports
    
    * chore: fix imports
    
    * chore: implement create client handler
    
    * chore: update core storage
    
    * chore: removed unused method
    
    * chore: code formating
    
    * chore: implement clientstate and consensus state
    
    * chore: implement default for types
    
    * chore: update cargo
    
    * chore: implement consensus state object
    
    * chore: removed unused imports
    
    * chore: update openinit handshake
    
    * chore: add create client test
    
    * chore: update consensus state decoding
    
    * chore: added validation
    
    * chore: fixed testcases
    
    * chore: fixed error
    
    * chore: fixed error
    
    * chore: update storage , update open_init and test-cases
    
    * chore: remove commented code
    
    ---------
    
    Co-authored-by: shreyasbhat0 <shreyas.ks@hugobyte.com>
    Co-authored-by: Shreyas S Bhat <35568964+shreyasbhat0@users.noreply.github.com>

---
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/handler.rs
@@ -0,0 +1,78 @@
+use super::*;
+
+impl<'a> CwIbcCoreContext<'a> {
+    pub fn connection_open_init(
+        &self,
+        deps: DepsMut,
+        message: MsgConnectionOpenInit,
+    ) -> Result<Response, ContractError> {
+        let connection_identifier = self.generate_connection_idenfier(deps.storage)?;
+
+        self.client_state(deps.storage, &message.client_id_on_a.clone())?;
+
+        let client_id = ClientId::from(message.client_id_on_a.clone());
+
+        self.check_for_connection(deps.as_ref().storage, client_id.clone())?;
+
+        let versions = match message.version {
+            Some(version) => {
+                if self.get_compatible_versions().contains(&version) {
+                    vec![version]
+                } else {
+                    return Err(ContractError::IbcConnectionError {
+                        error: (ConnectionError::EmptyVersions),
+                    });
+                }
+            }
+            None => self.get_compatible_versions(),
+        };
+
+        let connection_end = ConnectionEnd::new(
+            State::Init,
+            message.client_id_on_a,
+            message.counterparty.clone(),
+            versions,
+            message.delay_period,
+        );
+
+        self.update_connection_commitment(
+            deps.storage,
+            connection_identifier.clone(),
+            connection_end.clone(),
+        )?;
+        self.store_connection_to_client(
+            deps.storage,
+            client_id.clone(),
+            connection_identifier.clone(),
+        )?;
+        self.store_connection(deps.storage, connection_identifier.clone(), connection_end)
+            .unwrap();
+
+        let event = create_open_init_event(
+            connection_identifier.connection_id().as_str(),
+            client_id.as_str(),
+            message.counterparty.client_id().as_str(),
+        );
+
+        return Ok(Response::new()
+            .add_attribute("method", "connection_open_init")
+            .add_attribute("connection_id", connection_identifier.as_str())
+            .add_event(event));
+    }
+
+    pub fn generate_connection_idenfier(
+        &self,
+        store: &mut dyn Storage,
+    ) -> Result<ConnectionId, ContractError> {
+        let counter = self.connection_counter(store)?;
+
+        let connection_id = ConnectionId::new(counter);
+
+        self.increase_connection_counter(store)?;
+
+        Ok(connection_id)
+    }
+    pub fn get_compatible_versions(&self) -> Vec<Version> {
+        vec![Version::default()]
+    }
+}
--- contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/mod.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics03_connection/mod.rs
@@ -1,12 +1,22 @@
 //! ICS 03: Connection implementation for connecting a client
 pub mod connection;
 pub mod event;
+pub mod handler;
 use crate::context::CwIbcCoreContext;
+use crate::ics03_connection::event::create_open_init_event;
 use crate::types::{ClientId, ConnectionId};
 use crate::ContractError;
+use cosmwasm_std::DepsMut;
 use cosmwasm_std::Event;
+use cosmwasm_std::Response;
 use cosmwasm_std::Storage;
 use ibc::core::ics03_connection::connection::ConnectionEnd;
+use ibc::core::ics03_connection::error::ConnectionError;
+use ibc::core::ics03_connection::{
+    connection::{Counterparty, State},
+    msgs::conn_open_init::MsgConnectionOpenInit,
+    version::Version,
+};
 use ibc::core::ics23_commitment::commitment::CommitmentPrefix;
 use ibc::{
     core::ics03_connection::events::{
