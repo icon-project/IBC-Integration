9575774f4abd7341e557e1a6ea1a9cbb7cf6ab57
AntonAndell <anton@andell.eu>
2023-04-05 06:40:35 +0200

    feat: Add java integrations tests for channel establishment and closing (#207)
    Add mockApp to allow IBC handler to call relevant method hooks. Add a interface for IBC host and handler for cleaner tests.

--- contracts/javascore/lib/src/main/java/ibc/icon/interfaces/IIBCHandler.java
+++ contracts/javascore/lib/src/main/java/ibc/icon/interfaces/IIBCHandler.java
@@ -1,5 +1,26 @@
 package ibc.icon.interfaces;
 
+import java.math.BigInteger;
 
+import foundation.icon.score.client.ScoreClient;
+import score.Address;
+import score.annotation.External;
+
+@ScoreClient
 public interface IIBCHandler {
+    @External(readonly = true)
+    String name();
+
+    /**
+     * bindPort binds to an unallocated port, failing if the port has already
+     * been allocated.
+     */
+    @External
+    void bindPort(String portId, Address moduleAddress);
+
+    /**
+     * setExpectedTimePerBlock sets expected time per block.
+     */
+    @External
+    void setExpectedTimePerBlock(BigInteger expectedTimePerBlock);
 }
