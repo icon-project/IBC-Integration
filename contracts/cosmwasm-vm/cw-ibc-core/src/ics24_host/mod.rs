f2ce4bd9331234040f77d6e36dcc28ceb8c41d88
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-04-11 15:45:56 +0530

    chore: added unit testcases

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics24_host/mod.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics24_host/mod.rs
@@ -1,7 +1,7 @@
 //! ICS 24: Host defines the minimal set of interfaces that a
 //! state machine hosting an IBC-enabled chain must implement.
 pub mod commitment;
-pub mod IBCHost;
+pub mod host;
 
 use crate::context::CwIbcCoreContext;
 use ibc::core::ics24_host::path::*;
