cf8990e9be8853b1af06b76630cf33cf7fabf4b3
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-04-12 13:11:11 +0530

    feat: 103-implement-storage-for-host (#264)
    * chore: added storage
    
    * chore: added unit testcases
    
    * chore: update tests
    
    * fix: typo in error handling
    
    ---------
    
    Co-authored-by: Shreyas S Bhat <35568964+shreyasbhat0@users.noreply.github.com>
    Co-authored-by: shreyasbhat0 <shreyas.ks@hugobyte.com>

---
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics24_host/host.rs
@@ -0,0 +1,35 @@
+use cosmwasm_std::Storage;
+
+use crate::ContractError;
+
+use super::*;
+
+impl<'a> CwIbcCoreContext<'a> {
+    pub fn store_capability(
+        &self,
+        store: &mut dyn Storage,
+        name: Vec<u8>,
+        address: Vec<String>,
+    ) -> Result<(), ContractError> {
+        match self.ibc_store().capabilities().save(store, name, &address) {
+            Ok(_) => Ok(()),
+            Err(error) => Err(ContractError::IbcDecodeError {
+                error: format!("FailedToStore {}", error),
+            }),
+        }
+    }
+
+    pub fn get_capability(
+        &self,
+        store: &mut dyn Storage,
+        name: Vec<u8>,
+    ) -> Result<Vec<String>, ContractError> {
+        Ok(self
+            .ibc_store()
+            .capabilities()
+            .load(store, name)
+            .map_err(|_| ContractError::IbcDecodeError {
+                error: "CapabilityNotFound".into(),
+            })?)
+    }
+}
---
+++ contracts/cosmwasm-vm/cw-ibc-core/tests/test_host.rs
@@ -0,0 +1,36 @@
+use cosmwasm_std::to_vec;
+use cw_ibc_core::context::CwIbcCoreContext;
+pub mod setup;
+use setup::*;
+
+#[test]
+fn test_set_capability() {
+    let mut deps = deps();
+    let name = to_vec(&u128::default()).unwrap();
+    let address = vec!["helo".to_string()];
+    let contract = CwIbcCoreContext::default();
+    let result = contract.store_capability(&mut deps.storage, name, address);
+    assert_eq!(result.is_ok(), true)
+}
+
+#[test]
+#[should_panic(expected = "IbcDecodeError { error: \"CapabilityNotFound\" }")]
+fn test_get_capability_fail() {
+    let mut deps = deps();
+    let name = to_vec(&u128::default()).unwrap();
+    let contract = CwIbcCoreContext::default();
+    contract.get_capability(&mut deps.storage, name).unwrap();
+}
+
+#[test]
+fn test_get_capability() {
+    let mut deps = deps();
+    let name = to_vec(&u128::default()).unwrap();
+    let address = vec!["hello".to_string()];
+    let contract = CwIbcCoreContext::default();
+    contract
+        .store_capability(&mut deps.storage, name.clone(), address)
+        .unwrap();
+    let result = contract.get_capability(&mut deps.storage, name);
+    assert_eq!(result.is_ok(), true)
+}
