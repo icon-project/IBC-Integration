17ffe944560b90afd575e7273f887da960f157c2
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-04-12 18:07:06 +0530

    feat: implement storage modifiers for expected time per block

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics24_host/host.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics24_host/host.rs
@@ -1,4 +1,6 @@
-use cosmwasm_std::Storage;
+use std::time::Duration;
+
+use cosmwasm_std::{Storage, Timestamp};
 
 use crate::ContractError;
 
@@ -31,5 +33,30 @@ impl<'a> CwIbcCoreContext<'a> {
             .map_err(|_| ContractError::IbcDecodeError {
                 error: "CapabilityNotFound".into(),
             })?)
+    }
+    pub fn set_expected_time_per_block(
+        &self,
+        store: &mut dyn Storage,
+        expected_time_per_block: u64,
+    ) -> Result<(), ContractError> {
+        self.ibc_store()
+            .expected_time_per_block()
+            .save(store, &expected_time_per_block)?;
+
+        Ok(())
+    }
+
+    pub fn get_expected_time_per_block(&self, store: &dyn Storage) -> Result<u64, ContractError> {
+        match self.ibc_store().expected_time_per_block().may_load(store)? {
+            Some(time) => Ok(time),
+            None => Err(ContractError::IbcDecodeError {
+                error: "NotFound".to_string(),
+            }),
+        }
+    }
+
+    pub fn calculate_block_delay(timestamp: Timestamp) -> u64 {
+        //TODO :Validate Block delay
+        Duration::from_secs(timestamp.plus_seconds(300).seconds()).as_secs()
     }
 }
