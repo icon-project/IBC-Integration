e231cb3bb6fcd2859916da57cec2356da2688012
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-04-13 11:57:23 +0530

    feat: implement host storage methods(#270)
    * chore: added storage
    
    * chore: added unit testcases
    
    * chore: added methods
    
    * chore: added host storage methods
    
    * chore: added unit testcases
    
    * chore: update host methods
    
    ---------
    
    Co-authored-by: Shreyas S Bhat <35568964+shreyasbhat0@users.noreply.github.com>
    Co-authored-by: shreyasbhat0 <shreyas.ks@hugobyte.com>

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics24_host/host.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics24_host/host.rs
@@ -1,7 +1,3 @@
-use cosmwasm_std::Storage;
-
-use crate::ContractError;
-
 use super::*;
 
 impl<'a> CwIbcCoreContext<'a> {
@@ -31,5 +27,55 @@ impl<'a> CwIbcCoreContext<'a> {
             .map_err(|_| ContractError::IbcDecodeError {
                 error: "CapabilityNotFound".into(),
             })?)
+    }
+    pub fn claim_capability(
+        &self,
+        store: &mut dyn Storage,
+        name: Vec<u8>,
+        address: String,
+    ) -> Result<(), ContractError> {
+        self.ibc_store().capabilities().update(
+            store,
+            name.clone(),
+            |update| -> Result<_, ContractError> {
+                match update {
+                    Some(mut value) => {
+                        value.push(address);
+                        Ok(value)
+                    }
+                    None => Err(ContractError::IbcDecodeError {
+                        error: "CapabilityNotFound".into(),
+                    }),
+                }
+            },
+        )?;
+
+        Ok(())
+    }
+
+    pub fn authenticate_capability(
+        &self,
+        store: &mut dyn Storage,
+        info: MessageInfo,
+        name: Vec<u8>,
+    ) -> bool {
+        let caller = info.sender.to_string();
+        let capability = self.get_capability(store, name).unwrap();
+        if capability.contains(&caller) {
+            return true;
+        }
+        return false;
+    }
+
+    pub fn lookup_modules(
+        &self,
+        store: &mut dyn Storage,
+        name: Vec<u8>,
+    ) -> Result<Vec<String>, ContractError> {
+        let capabilities = self.get_capability(store, name)?;
+        if capabilities.len() == 0 {
+            return Err(ContractError::Unauthorized {});
+        }
+        Ok(capabilities)
     }
 }
