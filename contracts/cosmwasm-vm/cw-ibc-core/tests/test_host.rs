210308d32e9c5895230fe6a0a3afa8699d4602bc
Prathiksha-Nataraja <90592522+Prathiksha-Nataraja@users.noreply.github.com>
2023-04-13 16:17:33 +0530

    chore: added unit testcase

--- contracts/cosmwasm-vm/cw-ibc-core/tests/test_host.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/tests/test_host.rs
@@ -1,6 +1,12 @@
 use cosmwasm_std::to_vec;
 use cw_ibc_core::context::CwIbcCoreContext;
 pub mod setup;
+
+use ibc::core::ics24_host::validate::{
+    validate_channel_identifier, validate_client_identifier, validate_connection_identifier,
+    validate_identifier, validate_port_identifier,
+};
+
 use setup::*;
 
 #[test]
@@ -112,4 +118,88 @@ fn test_lookup_modules() {
 
     let result = contract.lookup_modules(&mut deps.storage, name);
     assert_eq!(result.is_ok(), true)
+}
+
+#[test]
+fn invalid_port_id_min() {
+    // invalid min port id
+    let id = validate_port_identifier("p");
+    assert!(id.is_err())
+}
+
+#[test]
+fn invalid_port_id_max() {
+    let id = validate_port_identifier(
+        "9anxkcme6je544d5lnj46zqiiiygfqzf8w4bjecbnyj4lj6s7zlpst67yln64tixp9anxkcme6je544d5lnj46zqiiiygfqzf8w4bjecbnyj4lj6s7zlpst67yln64tixp",
+    );
+    assert!(id.is_err())
+}
+
+#[test]
+fn valid_port_id() {
+    let id = validate_port_identifier("f8w4bjecbnyj4lj6s7zlpst67yln");
+    assert!(id.is_ok())
+}
+
+#[test]
+fn invalid_connection_id_min() {
+    let id = validate_connection_identifier("connect01");
+    assert!(id.is_err())
+}
+
+#[test]
+fn valid_connection_id() {
+    let id = validate_connection_identifier("connection");
+    assert!(id.is_ok())
+}
+
+#[test]
+fn invalid_channel_id_min() {
+    let id = validate_channel_identifier("channel");
+    assert!(id.is_err())
+}
+
+#[test]
+fn invalid_channel_id_max() {
+    // invalid channel id, string length is 65 (Max - 64)
+    let id = validate_channel_identifier(
+        "ihhankr30iy4nna65hjl2wjod7182io1t2s7u3ip3wqtbbn1sl0rgcntqc540r36r",
+    );
+    assert!(id.is_err())
+}
+
+#[test]
+fn parse_invalid_client_id_min() {
+    // invalid min client id (Min - 9)
+    let id = validate_client_identifier("client");
+    assert!(id.is_err())
+}
+
+#[test]
+fn valid_client_id() {
+    // Max - 64
+    let id = validate_client_identifier(
+        "f0isrs5enif9e4td3r2jcbxoevhz6u1fthn4aforq7ams52jn5m48eiesfht9ckp",
+    );
+    assert!(id.is_ok())
+}
+
+#[test]
+fn invalid_id_path_separator() {
+    // invalid id with path separator
+    let id = validate_identifier("id/1", 1, 10);
+    assert!(id.is_err())
+}
+
+#[test]
+fn invalid_id_chars() {
+    let id = validate_identifier("channel@01", 1, 10);
+    assert!(id.is_err())
+}
+
+#[test]
+fn invalid_id_empty() {
+    // id empty
+    let id = validate_identifier("", 1, 10);
+    assert!(id.is_err())
 }
