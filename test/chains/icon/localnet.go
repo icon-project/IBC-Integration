bf4f2444e07c7f839ef76640eb9f8652de44f26c
hemz10 <sph2740@example.com>
2023-04-18 11:10:23 +0530

    feat: add query contract

--- test/chains/icon/localnet.go
+++ test/chains/icon/localnet.go
@@ -336,7 +336,7 @@ func (c *IconLocalnet) DeployContract(ctx context.Context, keyName string) (cont
 
 // ExecuteContract implements chains.Chain
 func (c *IconLocalnet) ExecuteContract(ctx context.Context, contractAddress, keyName, methodName, param string) (context.Context, error) {
-	execMethodName, p := c.GetExecuteParam(ctx, methodName, param)
+	ctx, execMethodName, p := c.GetExecuteParam(ctx, methodName, param)
 	hash, err := c.getFullNode().ExecuteContract(ctx, contractAddress, execMethodName, c.keystorePath, p)
 	fmt.Printf("Transaction Hash: %s\n", hash)
 	return ctx, err
@@ -356,7 +356,14 @@ func (c *IconLocalnet) GetLastBlock(ctx context.Context) (context.Context, error
 
 // QueryContract implements chains.Chain
 func (c *IconLocalnet) QueryContract(ctx context.Context, contractAddress, methodName, params string) (context.Context, error) {
-	return ctx, nil
+	time.Sleep(2 * time.Second)
+
+	// get query msg
+	queryMsg := GetQueryParam(methodName)
+	output, err := c.getFullNode().QueryContract(ctx, contractAddress, queryMsg, "")
+	chains.Response = output
+	fmt.Printf("Response is : %s \n", output)
+	return ctx, err
 }
 
 func (c *IconLocalnet) BuildWallets(ctx context.Context, keyName string) error {
@@ -364,23 +371,29 @@ func (c *IconLocalnet) BuildWallets(ctx context.Context, keyName string) error {
 	return err
 }
 
-func (c *IconLocalnet) GetExecuteParam(ctx context.Context, methodName, params string) (execMethodName string, args string) {
+func (c *IconLocalnet) GetExecuteParam(ctx context.Context, methodName, params string) (context.Context, string, string) {
 	if strings.Contains(methodName, "set_admin") {
 		return c.SetAdminParams(ctx, methodName, params)
 	}
-	return "", ""
+	return ctx, "", ""
 }
 
 func GetQueryParam(methodName string) string {
 	return "admin"
 }
 
-func (c *IconLocalnet) SetAdminParams(ctx context.Context, methodaName, keyName string) (string, string) {
+func (c *IconLocalnet) SetAdminParams(ctx context.Context, methodaName, keyName string) (context.Context, string, string) {
+	var admins chains.Admins
 	executeMethodName := "setAdmin"
 	wallet, _ := c.BuildWallet(ctx, keyName, "")
 	addr := string(wallet.Address())
 	addr = strings.ReplaceAll(addr, `"`, "")
+	admins.Admin = map[string]string{
+		keyName: addr,
+	}
 	args := "_address=" + addr
 	fmt.Println(args)
-	return executeMethodName, args
+	return context.WithValue(ctx, chains.AdminKey("Admins"), chains.Admins{
+		Admin: admins.Admin,
+	}), executeMethodName, args
 }
