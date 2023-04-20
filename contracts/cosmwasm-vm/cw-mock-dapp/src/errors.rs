49575bf674c0a1375d53cd73e43a177d18d5c6d4
Shanith K K <shanith.kk@hugobyte.com>
2023-04-20 16:51:46 +0530

    chore: Implement DAPP

---
+++ contracts/cosmwasm-vm/cw-mock-dapp/src/errors.rs
@@ -0,0 +1,17 @@
+use super::*;
+
+#[derive(Error, Debug)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+    #[error("Unauthorized")]
+    Unauthorized {},
+    #[error("DecodeError {error}")]
+    DecodeError { error: String },
+    #[error("RollBackMessageMismatch {sequence}")]
+    RollBackMismatch { sequence: u64 },
+    #[error("RevertFromDAPP")]
+    RevertFromDAPP,
+    #[error("ModuleAddressNotFound")]
+    ModuleAddressNotFound,
+}
