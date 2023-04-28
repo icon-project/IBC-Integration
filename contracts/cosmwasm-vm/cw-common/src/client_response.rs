0fb3ed99b2d3088b08d9fa4dd735dd874072d5dd
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-04-28 10:32:35 +0530

    chore: remove unused imports

--- contracts/cosmwasm-vm/cw-common/src/client_response.rs
+++ contracts/cosmwasm-vm/cw-common/src/client_response.rs
@@ -1,4 +1,5 @@
 use super::*;
+
 #[cw_serde]
 pub struct CreateClientResponse {
     client_type: String,
--- contracts/cosmwasm-vm/cw-ibc-core/src/msg.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/msg.rs
@@ -1,15 +1,3 @@
-use ibc::{
-    core::{
-        ics03_connection::{msgs::conn_open_try::MsgConnectionOpenTry, version::Version},
-        ics04_channel::{channel::Order, msgs::acknowledgement::Acknowledgement, packet::Packet},
-    },
-    signer::Signer,
-};
-use ibc_proto::google::protobuf::Any;
-use ibc_proto::ibc::core::connection::v1::MsgConnectionOpenTry as RawMsgConnectionOpenTry;
-use serde::{Deserialize, Serialize};
-use std::{str::FromStr, time::Duration};
-
 use super::*;
 
 #[cw_serde]
