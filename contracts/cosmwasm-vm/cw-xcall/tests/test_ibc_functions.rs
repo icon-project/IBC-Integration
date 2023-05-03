fdb445c9b1bd62af8e64a20b132463e24b9f3d45
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-05-03 12:07:22 +0530

    test: unittest on ack success and failure on response

--- contracts/cosmwasm-vm/cw-xcall/tests/test_ibc_functions.rs
+++ contracts/cosmwasm-vm/cw-xcall/tests/test_ibc_functions.rs
@@ -6,6 +6,7 @@ use cosmwasm_std::{
 };
 use cw_common::types::{Ack, Address};
 use cw_common::xcall_msg::ExecuteMsg;
+use cw_xcall::types::response::CallServiceMessageResponse;
 use cw_xcall::{
     state::CwCallService,
     types::{message::CallServiceMessage, request::CallServiceMessageRequest},
@@ -652,4 +653,218 @@ fn fails_on_setting_timeout_height_unauthorized() {
     .unwrap();
 
     assert_eq!(response, 100)
+}
+
+#[test]
+fn test_ack_success_on_call_request() {
+    let mock_info = create_mock_info(&alice().to_string(), "umlg", 2000);
+
+    let data = CallServiceMessageRequest::new(
+        Address::from(mock_info.sender.as_str()),
+        alice().to_string(),
+        1,
+        vec![],
+        vec![1, 2, 3],
+    );
+
+    let message: CallServiceMessage = data.try_into().unwrap();
+
+    let timeout_block = IbcTimeoutBlock {
+        revision: 0,
+        height: 0,
+    };
+    let timeout = IbcTimeout::with_block(timeout_block);
+    let src = IbcEndpoint {
+        port_id: "our-port".to_string(),
+        channel_id: "channel-1".to_string(),
+    };
+
+    let dst = IbcEndpoint {
+        port_id: "their-port".to_string(),
+        channel_id: "channel-3".to_string(),
+    };
+
+    let message = to_binary(&message).unwrap();
+
+    let packet = IbcPacket::new(message, src, dst, 0, timeout);
+
+    let ack = cw_xcall::ack::on_ack_sucess(packet);
+
+    assert!(ack.is_ok())
+}
+
+#[test]
+#[should_panic(expected = "ParseErr")]
+fn test_ack_on_fails() {
+    let mock_info = create_mock_info(&alice().to_string(), "umlg", 2000);
+
+    let data = CallServiceMessageRequest::new(
+        Address::from(mock_info.sender.as_str()),
+        alice().to_string(),
+        1,
+        vec![],
+        vec![1, 2, 3],
+    );
+
+    let message: CallServiceMessage = data.try_into().unwrap();
+
+    let timeout_block = IbcTimeoutBlock {
+        revision: 0,
+        height: 0,
+    };
+    let timeout = IbcTimeout::with_block(timeout_block);
+    let src = IbcEndpoint {
+        port_id: "our-port".to_string(),
+        channel_id: "channel-1".to_string(),
+    };
+
+    let dst = IbcEndpoint {
+        port_id: "their-port".to_string(),
+        channel_id: "channel-3".to_string(),
+    };
+
+    let packet = IbcPacket::new(message, src, dst, 0, timeout);
+
+    cw_xcall::ack::on_ack_sucess(packet).unwrap();
+}
+
+#[test]
+fn test_ack_success_on_call_response() {
+    let data = CallServiceMessageResponse::new(
+        0,
+        cw_xcall::types::response::CallServiceResponseType::CallServiceResponseSuccess,
+        "Success",
+    );
+
+    let message: CallServiceMessage = data.try_into().unwrap();
+
+    let timeout_block = IbcTimeoutBlock {
+        revision: 0,
+        height: 0,
+    };
+    let timeout = IbcTimeout::with_block(timeout_block);
+    let src = IbcEndpoint {
+        port_id: "our-port".to_string(),
+        channel_id: "channel-1".to_string(),
+    };
+
+    let dst = IbcEndpoint {
+        port_id: "their-port".to_string(),
+        channel_id: "channel-3".to_string(),
+    };
+
+    let message = to_binary(&message).unwrap();
+
+    let packet = IbcPacket::new(message, src, dst, 0, timeout);
+
+    let ack = cw_xcall::ack::on_ack_sucess(packet);
+
+    assert!(ack.is_ok())
+}
+
+#[test]
+fn test_ack_failure_on_call_request() {
+    let mock_info = create_mock_info(&alice().to_string(), "umlg", 2000);
+
+    let data = CallServiceMessageRequest::new(
+        Address::from(mock_info.sender.as_str()),
+        alice().to_string(),
+        1,
+        vec![],
+        vec![1, 2, 3],
+    );
+
+    let message: CallServiceMessage = data.try_into().unwrap();
+
+    let timeout_block = IbcTimeoutBlock {
+        revision: 0,
+        height: 0,
+    };
+    let timeout = IbcTimeout::with_block(timeout_block);
+    let src = IbcEndpoint {
+        port_id: "our-port".to_string(),
+        channel_id: "channel-1".to_string(),
+    };
+
+    let dst = IbcEndpoint {
+        port_id: "their-port".to_string(),
+        channel_id: "channel-3".to_string(),
+    };
+
+    let message = to_binary(&message).unwrap();
+
+    let packet = IbcPacket::new(message, src, dst, 0, timeout);
+
+    let ack = cw_xcall::ack::on_ack_failure(packet, "Failed to Execute");
+
+    assert!(ack.is_ok())
+}
+
+#[test]
+#[should_panic(expected = "ParseErr")]
+fn fails_on_ack_failure_for_call_request() {
+    let mock_info = create_mock_info(&alice().to_string(), "umlg", 2000);
+
+    let data = CallServiceMessageRequest::new(
+        Address::from(mock_info.sender.as_str()),
+        alice().to_string(),
+        1,
+        vec![],
+        vec![1, 2, 3],
+    );
+
+    let message: CallServiceMessage = data.try_into().unwrap();
+
+    let timeout_block = IbcTimeoutBlock {
+        revision: 0,
+        height: 0,
+    };
+    let timeout = IbcTimeout::with_block(timeout_block);
+    let src = IbcEndpoint {
+        port_id: "our-port".to_string(),
+        channel_id: "channel-1".to_string(),
+    };
+
+    let dst = IbcEndpoint {
+        port_id: "their-port".to_string(),
+        channel_id: "channel-3".to_string(),
+    };
+
+    let packet = IbcPacket::new(message, src, dst, 0, timeout);
+
+    cw_xcall::ack::on_ack_failure(packet, "Failed to Execute").unwrap();
+}
+
+#[test]
+fn test_ack_failure_on_call_response() {
+    let data = CallServiceMessageResponse::new(
+        0,
+        cw_xcall::types::response::CallServiceResponseType::CallServiceResponseSuccess,
+        "Success",
+    );
+
+    let message: CallServiceMessage = data.try_into().unwrap();
+
+    let timeout_block = IbcTimeoutBlock {
+        revision: 0,
+        height: 0,
+    };
+    let timeout = IbcTimeout::with_block(timeout_block);
+    let src = IbcEndpoint {
+        port_id: "our-port".to_string(),
+        channel_id: "channel-1".to_string(),
+    };
+
+    let dst = IbcEndpoint {
+        port_id: "their-port".to_string(),
+        channel_id: "channel-3".to_string(),
+    };
+
+    let message = to_binary(&message).unwrap();
+
+    let packet = IbcPacket::new(message, src, dst, 0, timeout);
+
+    let ack = cw_xcall::ack::on_ack_failure(packet, "Failed to Execute");
+
+    assert!(ack.is_ok())
 }
