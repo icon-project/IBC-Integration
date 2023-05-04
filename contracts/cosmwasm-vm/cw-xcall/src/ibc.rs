d39a055745ea2d49ea9cc3b5db3f338621dd8c88
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-05-05 00:01:24 +0530

    add xcall docs

--- contracts/cosmwasm-vm/cw-xcall/src/ibc.rs
+++ contracts/cosmwasm-vm/cw-xcall/src/ibc.rs
@@ -1,9 +1,31 @@
 use super::*;
 
+/// These are constants used in the IBC (Inter-Blockchain Communication) protocol implementation in the
+/// Rust programming language.
 pub const IBC_VERSION: &str = "xcall-1";
 pub const APP_ORDER: IbcOrder = IbcOrder::Unordered;
 
-/// Handles the `OpenInit` and `OpenTry` parts of the IBC handshake.
+
+/// This function handles the opening of an IBC channel and performs some checks before returning a
+/// response.
+/// 
+/// Arguments:
+/// 
+/// * `_deps`: _deps is a mutable dependency injector that provides access to the necessary dependencies
+/// for the function to execute, such as the storage, API, and other modules.
+/// * `_env`: _env is an object that represents the current execution environment of the contract. It
+/// contains information such as the current block height, the sender address, the contract address, and
+/// the current time.
+/// * `msg`: The `msg` parameter is of type `IbcChannelOpenMsg`, which is a struct that contains
+/// information about the channel being opened in an IBC transaction. It includes details such as the
+/// channel's order (whether it is ordered or unordered), the counterparty's version (if applicable),
+/// and
+/// 
+/// Returns:
+/// 
+/// a `Result` containing an `IbcChannelOpenResponse` or a `ContractError`. The `IbcChannelOpenResponse`
+/// is wrapped in an `Ok` variant and contains an `Ibc3ChannelOpenResponse` struct with a `version`
+/// field set to a string representing the IBC version.
 #[cfg_attr(feature = "native_ibc", entry_point)]
 pub fn ibc_channel_open(
     _deps: DepsMut,
@@ -23,6 +45,22 @@ pub fn ibc_channel_open(
     }))
 }
 
+/// This function connects two IBC channels and saves their configuration.
+/// 
+/// Arguments:
+/// 
+/// * `deps`: `deps` is a mutable reference to the dependencies of the contract. It is used to interact
+/// with the blockchain state and perform operations such as reading and writing to storage, querying
+/// the current block height, and sending messages to other contracts.
+/// * `_env`: _env is an input parameter of type `Env` which represents the current blockchain
+/// environment. It contains information such as the current block height, time, and chain ID.
+/// * `msg`: The `msg` parameter is of type `IbcChannelConnectMsg`, which contains information about the
+/// channel to be connected, including the channel order, counterparty version, source endpoint, and
+/// destination endpoint.
+/// 
+/// Returns:
+/// 
+/// a `Result` with either an `IbcBasicResponse` or a `ContractError`.
 #[cfg_attr(feature = "native_ibc", entry_point)]
 pub fn ibc_channel_connect(
     deps: DepsMut,
@@ -47,6 +85,23 @@ pub fn ibc_channel_connect(
     Ok(IbcBasicResponse::new().add_attribute("method", "ibc_channel_connect"))
 }
 
+/// This Rust function handles closing an IBC channel and resets its state.
+/// 
+/// Arguments:
+/// 
+/// * `_deps`: DepsMut is a mutable dependency container that provides access to the necessary
+/// dependencies required for executing the contract code. These dependencies include the storage, API,
+/// and other modules required for the contract to function properly.
+/// * `_env`: _env is an object of type `Env` which represents the current execution environment of the
+/// contract. It contains information such as the current block height, time, and chain ID.
+/// * `msg`: The `msg` parameter is of type `IbcChannelCloseMsg`, which is a struct that contains
+/// information about the channel being closed. It includes the channel endpoint, which contains the
+/// channel ID and the port ID, as well as any additional attributes that were included in the close
+/// message.
+/// 
+/// Returns:
+/// 
+/// a `Result` object that contains either an `IbcBasicResponse` or a `ContractError`.
 #[cfg_attr(feature = "native_ibc", entry_point)]
 pub fn ibc_channel_close(
     _deps: DepsMut,
@@ -61,6 +116,26 @@ pub fn ibc_channel_close(
         .add_attribute("channel", channel))
 }
 
+/// This function receives an IBC packet and returns a response or an error message.
+/// 
+/// Arguments:
+/// 
+/// * `deps`: `deps` is a mutable dependency injector that provides access to the necessary dependencies
+/// for executing the function. It is used to access the necessary modules and traits required for the
+/// function to execute properly.
+/// * `env`: `env` is an object of type `Env` which contains information about the current blockchain
+/// environment, such as the block height, time, and chain ID. It is used in the `ibc_packet_receive`
+/// function to access this information when processing IBC packets.
+/// * `msg`: The `msg` parameter is of type `IbcPacketReceiveMsg` and contains the data of the IBC
+/// packet being received. It includes information such as the source and destination chain IDs, the
+/// packet sequence, and the actual packet data.
+/// 
+/// Returns:
+/// 
+/// The function `ibc_packet_receive` returns a `Result` with either an `IbcReceiveResponse` if the
+/// `do_ibc_packet_receive` function call is successful, or a `Never` type if there is an error. If
+/// there is an error, the function returns an `IbcReceiveResponse` with an error message and a failed
+/// acknowledgement.
 #[cfg_attr(feature = "native_ibc", entry_point)]
 pub fn ibc_packet_receive(
     deps: DepsMut,
@@ -75,6 +150,21 @@ pub fn ibc_packet_receive(
             .set_ack(make_ack_fail(error.to_string()))),
     }
 }
+/// This function receives an IBC packet and calls a service to handle the packet data.
+/// 
+/// Arguments:
+/// 
+/// * `deps`: `deps` is a mutable reference to the dependencies of the contract. These dependencies
+/// include the storage, API, and other modules that the contract may need to interact with.
+/// * `_env`: _env is an object of type `Env` which represents the current execution environment of the
+/// contract. It contains information such as the current block height, time, and chain ID.
+/// * `msg`: The `msg` parameter is of type `IbcPacketReceiveMsg`, which represents a message containing
+/// an IBC packet that has been received by the contract. It contains information such as the packet
+/// data, the source and destination channels, and the sequence numbers of the packets.
+/// 
+/// Returns:
+/// 
+/// a `Result` with either an `IbcReceiveResponse` or a `ContractError`.
 
 fn do_ibc_packet_receive(
     deps: DepsMut,
@@ -87,6 +177,22 @@ fn do_ibc_packet_receive(
     call_service.receive_packet_data(deps, msg.packet)
 }
 
+/// This function handles the acknowledgement of an IBC packet in Rust.
+/// 
+/// Arguments:
+/// 
+/// * `_deps`: _deps is a mutable dependency injector that provides access to the necessary dependencies
+/// for the function to execute, such as the storage and API interfaces.
+/// * `_env`: _env is a variable of type Env which represents the current execution environment of the
+/// contract. It contains information such as the current block height, the sender address, and the
+/// current time. It is passed as a parameter to the ibc_packet_ack function.
+/// * `ack`: `ack` is a parameter of type `IbcPacketAckMsg`, which represents the acknowledgement
+/// message for an IBC packet. It contains information about the original packet that was sent and the
+/// acknowledgement data received in response.
+/// 
+/// Returns:
+/// 
+/// a `Result` object with an `IbcBasicResponse` on success or a `ContractError` on failure.
 #[cfg_attr(feature = "native_ibc", entry_point)]
 pub fn ibc_packet_ack(
     _deps: DepsMut,
@@ -101,6 +207,23 @@ pub fn ibc_packet_ack(
     }
 }
 
+/// This Rust function handles a timeout for an IBC packet and sends a reply message with an error code.
+/// 
+/// Arguments:
+/// 
+/// * `_deps`: _deps is a mutable dependency injector that provides access to the necessary dependencies
+/// for executing the function. It is typically used to access the storage, API, and other modules
+/// required for the function to execute.
+/// * `_env`: _env is an object of type Env which contains information about the current blockchain
+/// environment, such as the block height, time, and chain ID. It is used in this function to provide
+/// context for the IBC packet timeout message.
+/// * `_msg`: The _msg parameter is of type IbcPacketTimeoutMsg, which represents a message indicating
+/// that a previously sent IBC packet has timed out and failed to be acknowledged by the receiving
+/// chain.
+/// 
+/// Returns:
+/// 
+/// a `Result` with an `IbcBasicResponse` on success or a `ContractError` on failure.
 #[cfg_attr(feature = "native_ibc", entry_point)]
 pub fn ibc_packet_timeout(
     _deps: DepsMut,
--- contracts/cosmwasm-vm/cw-xcall/src/msg.rs
+++ contracts/cosmwasm-vm/cw-xcall/src/msg.rs
@@ -1,13 +1,29 @@
 use super::*;
 
+/// This is a Rust struct representing a message to instantiate a contract with timeout height and IBC
+/// host address.
+/// 
+/// Properties:
+/// 
+/// * `timeout_height`: `timeout_height` is a field of type `u64` (unsigned 64-bit integer) in the
+/// `InstantiateMsg` struct. It represents the block height at which the transaction will timeout if it
+/// has not been included in a block by that height. This is used to prevent transactions from being
+/// * `ibc_host`: `ibc_host` is a field of type `Addr` in the `InstantiateMsg` struct. It likely
+/// represents the address of the IBC host that the message is being sent to. However, without more
+/// context it's difficult to say for sure.
 #[cw_serde]
 pub struct InstantiateMsg {
     pub timeout_height: u64,
     pub ibc_host: Addr,
 }
 
+/// The `#[cw_serde]` attribute is used to automatically generate serialization and deserialization code
+/// for the struct or enum it is applied to.
 #[cw_serde]
 #[derive(QueryResponses)]
+/// This is a Rust enum representing different types of queries that can be made to the contract. Each
+/// variant of the enum corresponds to a specific query and has a return type specified using the
+/// `#[returns]` attribute.
 pub enum QueryMsg {
     #[returns(String)]
     GetAdmin {},
