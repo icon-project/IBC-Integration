86ea5aa91a2d82556f9052f0fc0c53a49668080f
AntonAndell <anton@andell.eu>
2023-05-15 08:59:17 +0200

    refactor: Review fixes
    Optimise branch db acess. Remove redundant dappProxy class.

--- contracts/javascore/xcall-multi-protocol/src/main/java/foundation/icon/btp/xcall/CallServiceImpl.java
+++ contracts/javascore/xcall-multi-protocol/src/main/java/foundation/icon/btp/xcall/CallServiceImpl.java
@@ -44,7 +44,7 @@ public class CallServiceImpl implements CallService, FeeManage {
     private final DictDB<BigInteger, CallRequest> requests = Context.newDictDB("requests", CallRequest.class);
     private final DictDB<BigInteger, CSMessageRequest> proxyReqs = Context.newDictDB("proxyReqs", CSMessageRequest.class);
     private final BranchDB<byte[], DictDB<String, Boolean>> pendingReqs = Context.newBranchDB("pendingReqs", Boolean.class);
-    private final BranchDB<byte[], DictDB<String, Boolean>> pendingResponses = Context.newBranchDB("pendingResponses", Boolean.class);
+    private final BranchDB<BigInteger, DictDB<String, Boolean>> pendingResponses = Context.newBranchDB("pendingResponses", Boolean.class);
 
     // for fee-related operations
     private final VarDB<Address> admin = Context.newVarDB("admin", Address.class);
@@ -178,7 +178,7 @@ public class CallServiceImpl implements CallService, FeeManage {
 
         CSMessageResponse msgRes = null;
         try {
-            DAppProxy proxy = new DAppProxy(req.getFrom());
+            CallServiceReceiver proxy = new CallServiceReceiverScoreInterface(req.getFrom());
             proxy.handleCallMessage(getNetworkAddress(), req.getRollback(), req.getProtocols());
             msgRes = new CSMessageResponse(_sn, CSMessageResponse.SUCCESS, "");
         } catch (UserRevertedException e) {
@@ -258,22 +258,22 @@ public class CallServiceImpl implements CallService, FeeManage {
         if (protocols.length > 1) {
             byte[] hash = Context.hash("sha-256", data);
             String source = Context.getCaller().toString();
-            pendingReqs.at(hash).set(source, true);
+            DictDB<String,Boolean> pendingRequests = pendingReqs.at(hash);
+            pendingRequests.set(source, true);
             for (String protocol : protocols) {
-                if (!pendingReqs.at(hash).getOrDefault(protocol, false)) {
+                if (!pendingRequests.getOrDefault(protocol, false)) {
                     return;
                 }
             }
 
             for (String protocol : protocols) {
-                pendingReqs.at(hash).set(protocol, null);
+                pendingRequests.set(protocol, null);
             }
         }
 
         String to = msgReq.getTo();
         BigInteger reqId = getNextReqId();
-        CSMessageRequest req = new CSMessageRequest(msgReq.getFrom(), to, msgReq.getProtocols(), msgReq.getSn(), msgReq.needRollback(), msgReq.getData());
-        proxyReqs.set(reqId, req);
+        proxyReqs.set(reqId, msgReq);
 
         // emit event to notify the user
         CallMessage(msgReq.getFrom(), to, msgReq.getSn(), reqId);
@@ -290,17 +290,17 @@ public class CallServiceImpl implements CallService, FeeManage {
 
         String[] protocols = req.getProtocols();
         if (protocols.length > 1) {
-            byte[] hash = Context.hash("sha-256", data);
             String source = Context.getCaller().toString();
-            pendingResponses.at(hash).set(source, true);
+            DictDB<String,Boolean> pendingResponse = pendingResponses.at(resSn);
+            pendingResponse.set(source, true);
             for (String protocol : protocols) {
-                if (!pendingResponses.at(hash).getOrDefault(protocol, false)) {
+                if (!pendingResponse.getOrDefault(protocol, false)) {
                     return;
                 }
             }
 
             for (String protocol : protocols) {
-                pendingResponses.at(hash).set(protocol, null);
+                pendingResponse.set(protocol, null);
             }
         }
 
