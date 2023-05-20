eafaf9ab3aa7bfa87fe28b201c306afb5e538760
sabinchitrakar <immortal.infidel@gmail.com>
2023-05-20 21:36:30 +0545

    fix: remove unwanted file

--- libraries/rust/common/src/ibc/mock/mod.rs
+++ libraries/rust/common/src/ibc/mock/mod.rs
@@ -1,15 +1,6 @@
 //! Implementation of mocks for context, host chain, and client.
 
 pub mod client_state;
-
 pub mod consensus_state;
-// #[cfg(test)]
-// pub mod context;
-
 pub mod header;
-// #[cfg(test)]
-// pub mod host;
-// #[cfg(test)]
-// pub mod ics18_relayer;
-
 pub mod misbehaviour;
