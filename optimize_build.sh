26989d10b942de035b5503d92a3ba7fcea5e48ee
Night Owl <nightowl121@protonmail.com>
2023-05-31 15:41:15 +0545

    chore: use updated version of repository checkout
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- optimize_build.sh
+++ optimize_build.sh
@@ -14,23 +14,26 @@ cargo clean
 rustup target add wasm32-unknown-unknown
 cargo install cosmwasm-check
 if ! which wasm-opt; then
-    curl -OL $BINARYEN_DWN
-    tar xf binaryen-version_${BINARYEN_VERS}-x86_64-linux.tar.gz
-    export PATH=$PATH:$PWD/binaryen-version_${BINARYEN_VERS}/bin
+  curl -OL $BINARYEN_DWN
+  tar xf binaryen-version_${BINARYEN_VERS}-x86_64-linux.tar.gz
+  export PATH=$PATH:$PWD/binaryen-version_${BINARYEN_VERS}/bin
 fi
 
-RUSTFLAGS='-C link-arg=-s' cargo build  --workspace --exclude test-utils --release --lib --target wasm32-unknown-unknown
+wasm-opt --version
+
+RUSTFLAGS='-C link-arg=-s' cargo build --workspace --exclude test-utils --release --lib --target wasm32-unknown-unknown
 for WASM in ./target/wasm32-unknown-unknown/release/*.wasm; do
-    NAME=$(basename "$WASM" .wasm)${SUFFIX}.wasm
-    echo "Creating intermediate hash for $NAME ..."
-    sha256sum -- "$WASM" | tee -a artifacts/checksums_intermediate.txt
-    echo "Optimizing $NAME ..."
-    wasm-opt -Oz "$WASM"   -o "artifacts/$NAME"
-  done
+  NAME=$(basename "$WASM" .wasm)${SUFFIX}.wasm
+  echo "Creating intermediate hash for $NAME ..."
+  sha256sum -- "$WASM" | tee -a artifacts/checksums_intermediate.txt
+  echo "Optimizing $NAME ..."
+  wasm-opt -Oz "$WASM" -o "artifacts/$NAME"
+done
+
+# check all generated wasm files
 cosmwasm-check artifacts/cw_ibc_core.wasm
 cosmwasm-check artifacts/cw_icon_light_client.wasm
 cosmwasm-check artifacts/cw_mock_dapp.wasm
 cosmwasm-check artifacts/cw_xcall.wasm
 cosmwasm-check artifacts/cw_xcall_ibc_connection.wasm
 cosmwasm-check artifacts/cw_xcall_app.wasm
-
--- scripts/deploy_cosmwasm.sh
+++ scripts/deploy_cosmwasm.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+
+set -ex
 
 WALLET=fd # This account is default while running the chain on docker.
 ### LOCALNET ###
@@ -6,23 +8,23 @@ ENDPOINT=http://localhost:26657
 CHAIN_ID=localnet
 TOKEN=stake
 
-deploy_wasm(){
-	RES=$(archwayd tx wasm store $CONTRACT_WASM \
-		--keyring-backend test \
-		--from $WALLET \
-		--node $ENDPOINT \
-		--chain-id $CHAIN_ID \
-		--gas-prices 0.02$TOKEN \
-		--gas auto \
-		--gas-adjustment 1.3 -y \
-		--output json -b block)
-	
-	echo "Result: "
-	echo $RES 
+deploy_wasm() {
+  RES=$(archwayd tx wasm store $CONTRACT_WASM \
+    --keyring-backend test \
+    --from $WALLET \
+    --node $ENDPOINT \
+    --chain-id $CHAIN_ID \
+    --gas-prices 0.02$TOKEN \
+    --gas auto \
+    --gas-adjustment 1.3 -y \
+    --output json -b block)
+
+  echo "Result: "
+  echo "$RES"
 }
 
 # This wasm directory is inside the docker container
 for CONTRACT_WASM in /contracts/artifacts/*.wasm; do
-	echo "=> Deploying $CONTRACT_WASM"
-	deploy_wasm $CONTRACT_WASM
+  echo "=> Deploying $CONTRACT_WASM"
+  deploy_wasm "$CONTRACT_WASM"
 done
