473c3bc05f8bee01d1eb02652d96e8705cd4c6fc
sabinchitrakar <immortal.infidel@gmail.com>
2023-07-05 12:08:00 +0545

    fix: data swapped

--- contracts/cosmwasm-vm/cw-xcall-ibc-connection/src/receive_packet.rs
+++ contracts/cosmwasm-vm/cw-xcall-ibc-connection/src/receive_packet.rs
@@ -53,7 +53,7 @@ impl<'a> CwIbcConnection<'a> {
             }
         }
         debug_println!("[IBCConnection]: forwarding to xcall");
-        let data = message.data.0;
+        let data = n_message.data;
         let xcall_submessage =
             self.call_xcall_handle_message(deps.storage, &nid, data, n_message.sn.0)?;
 
