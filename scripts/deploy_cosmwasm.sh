902542ee332e699416f9333619c612552cd4d9d4
DeepakBomjan <dpaktamang@gmail.com>
2023-07-10 22:48:15 +0545

    ci: check archway testnet deployment

--- scripts/deploy_cosmwasm.sh
+++ scripts/deploy_cosmwasm.sh
@@ -2,11 +2,31 @@
 
 set -e
 
-WALLET=fd # This account is default while running the chain on docker.
-### LOCALNET ###
-ENDPOINT=http://localhost:26657
-CHAIN_ID=localnet
-TOKEN=stake
+print_usage() {
+    echo "Usage: $0 [local|testnet]"
+    echo "Description: This script requires deployment type as an argument."
+}
+
+
+if [ $# -eq 0 ]; then
+    print_usage
+    exit 1
+fi
+
+if [ "$1" == "local" ]; then
+  ENDPOINT=http://localhost:26657
+  CHAIN_ID=localnet
+  WALLET=fd # This account is default while running the chain on docker.
+  TOKEN=stake
+elif [ "$1" == "testnet" ]
+  ENDPOINT=https://rpc.constantine.archway.tech:443
+  CHAIN_ID=constantine-3
+  WALLET=constantine2_wallet
+  TOKEN=const
+else
+  print_usage
+  exit 1
+fi
 
 deploy_wasm() {
   RES=$(archwayd tx wasm store $CONTRACT_WASM \
@@ -26,5 +46,6 @@ deploy_wasm() {
 # This wasm directory is inside the docker container
 for CONTRACT_WASM in /contracts/artifacts/*.wasm; do
   echo "=> Deploying $CONTRACT_WASM"
-  deploy_wasm "$CONTRACT_WASM"
+  deploy_wasm "$CONTRACT_WASM" 
 done
+
