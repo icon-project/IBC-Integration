3f18497751404fa5a1a0e5b41672ffaf120a4db4
AntonAndell <anton@andell.eu>
2023-07-17 13:27:28 +0200

    adapt test to xCall gas optimization change

--- test/e2e/tests/xcall.go
+++ test/e2e/tests/xcall.go
@@ -2,12 +2,12 @@ package tests
 
 import (
 	"context"
-	"fmt"
+	"testing"
+
+	"github.com/icon-project/ibc-integration/test/chains"
 	"github.com/icon-project/ibc-integration/test/e2e/testsuite"
 	"github.com/strangelove-ventures/interchaintest/v7/ibc"
 	"github.com/stretchr/testify/assert"
-	"testing"
-	"time"
 )
 
 type XCallTestSuite struct {
@@ -19,32 +19,42 @@ func (x *XCallTestSuite) TestDemo() {
 	portId := "transfer"
 	ctx := context.TODO()
 	x.SetupXCall(ctx, portId)
+	x.DeployMockApp(ctx, portId)
 
-	x.T.Run("xcall test", func(t *testing.T) {
-		x.DeployMockApp(ctx, portId)
+	x.T.Run("xcall one way message", func(t *testing.T) {
 		chainA, chainB := x.GetChains()
-		msg := "Hello"
-		dst := chainB.(ibc.Chain).Config().ChainID + "/" + chainB.GetIBCAddress("dapp")
-		_, reqId, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, []byte(msg), nil)
-		x.Require().NoError(err)
-		ctx, err = chainB.ExecuteCall(ctx, reqId)
-		fmt.Println(ctx.Value("txResult"))
-
-		msg = "rollback"
-		rollback := "rollback data"
-		sn, reqId, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, []byte(msg), []byte(rollback))
-
-		ctx, err = chainB.ExecuteCall(ctx, reqId)
-		fmt.Println(ctx.Value("txResult"))
-		time.Sleep(10 * time.Second)
-
-		ctx, err = chainA.ExecuteRollback(ctx, sn)
-		fmt.Println(ctx.Value("txResult"))
+		x.TestOneWayMessage(ctx, t, chainA, chainB)
+		x.TestOneWayMessage(ctx, t, chainB, chainA)
 	})
 
-	x.T.Run("another test", func(t *testing.T) {
-		assertions := assert.New(t)
-		assertions.Equal(123, 123, "they should be equal")
+	x.T.Run("xcall test rollback", func(t *testing.T) {
+		chainA, chainB := x.GetChains()
+		x.TestRollback(ctx, t, chainA, chainB)
+		x.TestRollback(ctx, t, chainB, chainA)
 	})
+}
 
+func (x *XCallTestSuite) TestOneWayMessage(ctx context.Context, t *testing.T, chainA, chainB chains.Chain) {
+	msg := "Hello"
+	dst := chainB.(ibc.Chain).Config().ChainID + "/" + chainB.GetIBCAddress("dapp")
+	_, reqId, data, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, []byte(msg), nil)
+	x.Require().NoError(err)
+	ctx, err = chainB.ExecuteCall(ctx, reqId, data)
+	x.Require().NoError(err)
+}
+
+func (x *XCallTestSuite) TestRollback(ctx context.Context, t *testing.T, chainA, chainB chains.Chain) {
+	msg := "rollback"
+	rollback := "rollback data"
+	dst := chainB.(ibc.Chain).Config().ChainID + "/" + chainB.GetIBCAddress("dapp")
+	sn, reqId, data, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, []byte(msg), []byte(rollback))
+	x.Require().NoError(err)
+	height, err := chainA.(ibc.Chain).Height(ctx)
+	x.Require().NoError(err)
+	ctx, err = chainB.ExecuteCall(ctx, reqId, data)
+	code, msg, err := chainA.FindCallResponse(ctx, int64(height), sn)
+	x.Require().NoError(err)
+	assert.Equal(t, "-1", code)
+	ctx, err = chainA.ExecuteRollback(ctx, sn)
+	x.Require().NoError(err)
 }
