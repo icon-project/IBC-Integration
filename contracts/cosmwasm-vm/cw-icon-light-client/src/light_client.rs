ad26cc07de6bc5179bed81e8308d479ce49a78aa
sabinchitrakar <immortal.infidel@gmail.com>
2023-07-21 14:58:23 +0545

    chore: build check

--- contracts/cosmwasm-vm/cw-icon-light-client/src/light_client.rs
+++ contracts/cosmwasm-vm/cw-icon-light-client/src/light_client.rs
@@ -156,9 +156,9 @@ impl ILightClient for IconClient<'_> {
                 update_height: btp_header.main_height,
             });
         }
-       
-        if  (state.latest_height-btp_header.main_height) > state.trusting_period {
-            return Err(ContractError::UpdateBlockTooOld)
+
+        if (state.latest_height - btp_header.main_height) > state.trusting_period {
+            return Err(ContractError::UpdateBlockTooOld);
         }
 
         if state.network_id != btp_header.network_id {
