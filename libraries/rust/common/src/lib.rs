bd11b4a3e47bf7007143762df41486632a325fa6
ibrizsabin <101165234+ibrizsabin@users.noreply.github.com>
2023-07-26 18:47:55 +0545

    fix: cosmwasm-storage (#530)
    * fix: raw save
    
    * fix: fix deps
    
    * fix: refactor commitments
    
    * fix: update branch
    
    * fix: remove stringbyte conversion in verify membership
    
    * chore: pass build
    
    * fix: code formatting issue
    
    ---------
    
    Co-authored-by: sabinchitrakar <immortal.infidel@gmail.com>
    Co-authored-by: viveksharmapoudel <viveksharmapoudel@gmail.com>
    Co-authored-by: red__larva <red.x.larva@protonmail.com>

--- libraries/rust/common/src/lib.rs
+++ libraries/rust/common/src/lib.rs
@@ -112,6 +112,7 @@ impl<'a> PrimaryKey<'a> for ModuleId {
 
 impl KeyDeserialize for ModuleId {
     type Output = ModuleId;
+
     fn from_vec(value: Vec<u8>) -> cosmwasm_std::StdResult<Self::Output> {
         let result = String::from_utf8(value)
             .map_err(StdError::invalid_utf8)
@@ -134,6 +135,7 @@ impl<'a> PrimaryKey<'a> for PortId {
 
 impl KeyDeserialize for PortId {
     type Output = PortId;
+
     fn from_vec(value: Vec<u8>) -> cosmwasm_std::StdResult<Self::Output> {
         let result = String::from_utf8(value)
             .map_err(StdError::invalid_utf8)
