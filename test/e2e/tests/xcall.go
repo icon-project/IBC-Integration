d7b9cfbd725681371bf660ebe4adab01494c2ed8
Sudeep Bhandari <bhandari.sudeep68@gmail.com>
2023-07-27 11:16:07 +0545

    fix: happy path testing (#554)
    * add message bytes converter for byte messages to support both wasm and goloop
    
    * rollbackMessage
    
    * validateMessage without rollback
    
    * update chainType to wasm to support wasm chain focusing neutron link
    
    * -Update `StartBtpHeight` to `StartHeight` on configurations
    - Provide `StartHeight` as parameters on relay config
    
    * add BlockInterval as params on wasm chain configurations
    
    * add BlockInterval as params on wasm chain configurations
    
    * update chainType to `wasm` to support wasm chain focusing `neutron` link
    
    * Two way Happy Path Testing

--- test/e2e/tests/xcall.go
+++ test/e2e/tests/xcall.go
@@ -2,6 +2,9 @@ package tests
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/icon-project/ibc-integration/test/chains"
@@ -35,17 +38,23 @@ func (x *XCallTestSuite) TestDemo() {
 }
 
 func (x *XCallTestSuite) TestOneWayMessage(ctx context.Context, t *testing.T, chainA, chainB chains.Chain) {
-	msg := "Hello"
+	msg := "MessageTransferTestingWithoutRollback"
 	dst := chainB.(ibc.Chain).Config().ChainID + "/" + chainB.GetIBCAddress("dapp")
 	_, reqId, data, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, []byte(msg), nil)
 	x.Require().NoError(err)
 	ctx, err = chainB.ExecuteCall(ctx, reqId, data)
 	x.Require().NoError(err)
+
+	dataOutput, err := convertToPlainString(data)
+	x.Require().NoError(err)
+
+	assert.Equal(t, msg, dataOutput)
+	fmt.Println("Data Transfer Testing Without Rollback from " + chainA.(ibc.Chain).Config().ChainID + " to " + chainB.(ibc.Chain).Config().ChainID + " with data " + msg + " and Received:" + dataOutput + " PASSED")
 }
 
 func (x *XCallTestSuite) TestRollback(ctx context.Context, t *testing.T, chainA, chainB chains.Chain) {
-	msg := "rollback"
-	rollback := "rollback data"
+	msg := "MessageTransferTestingWithRollback"
+	rollback := "RollbackDataTesting"
 	dst := chainB.(ibc.Chain).Config().ChainID + "/" + chainB.GetIBCAddress("dapp")
 	sn, reqId, data, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, []byte(msg), []byte(rollback))
 	x.Require().NoError(err)
@@ -57,4 +66,41 @@ func (x *XCallTestSuite) TestRollback(ctx context.Context, t *testing.T, chainA,
 	assert.Equal(t, "-1", code)
 	ctx, err = chainA.ExecuteRollback(ctx, sn)
 	x.Require().NoError(err)
+}
+
+func convertToPlainString(input string) (string, error) {
+	if strings.HasPrefix(input, "0x") {
+		input = input[2:]
+	}
+
+	if strings.HasPrefix(input, "[") && strings.HasSuffix(input, "]") {
+		input = input[1 : len(input)-1]
+
+		parts := strings.Split(input, ", ")
+		var plainString strings.Builder
+		for _, part := range parts {
+			value, err := strconv.Atoi(part)
+			if err != nil {
+				return "", err
+			}
+			plainString.WriteByte(byte(value))
+		}
+
+		return plainString.String(), nil
+	}
+
+	if len(input)%2 != 0 {
+		return "", fmt.Errorf("invalid input length")
+	}
+
+	var plainString strings.Builder
+	for i := 0; i < len(input); i += 2 {
+		value, err := strconv.ParseUint(input[i:i+2], 16, 8)
+		if err != nil {
+			return "", err
+		}
+		plainString.WriteByte(byte(value))
+	}
+
+	return plainString.String(), nil
 }
