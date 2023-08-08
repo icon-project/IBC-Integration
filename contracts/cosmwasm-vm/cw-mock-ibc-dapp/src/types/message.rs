5812b1486ebea08cc559a80c8cdbcf8db8f02165
viveksharmapoudel <viveksharmapoudel@gmail.com>
2023-08-08 12:10:28 +0545

    fix: remove unwanted test from mock ibc dapp

--- contracts/cosmwasm-vm/cw-mock-ibc-dapp/src/types/message.rs
+++ contracts/cosmwasm-vm/cw-mock-ibc-dapp/src/types/message.rs
@@ -21,35 +21,3 @@ impl Decodable for Message {
         })
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use common::rlp::{self, Nullable};
-
-    use super::Message;
-
-    #[test]
-    fn test_rlp_decoding() {
-        let message = Message {
-            sn: Nullable::new(Some(150)),
-            data: hex::decode("74657374").unwrap(),
-        };
-        let rlp_bytes = rlp::encode(&message);
-
-        assert_eq!(
-            "d282009689008ac7230489e800008474657374",
-            hex::encode(rlp_bytes)
-        );
-
-        let message = Message {
-            sn: Nullable::new(None),
-            data: hex::decode("7465737432").unwrap(),
-        };
-        let rlp_bytes = rlp::encode(&message);
-
-        assert_eq!(
-            "d2f800890ad78ebc5ac6200000857465737432",
-            hex::encode(rlp_bytes)
-        )
-    }
-}
