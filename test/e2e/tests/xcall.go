b40d98171c0865cc66bf652ccbdeb861e0677951
sdpbhandari <bhandari.sudeep68@gmail.com>
2023-08-09 13:22:52 +0545

    Add tests for MaxSizeLimit from Chain A to ChainB and vice-versa

--- test/e2e/tests/xcall.go
+++ test/e2e/tests/xcall.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -43,6 +44,19 @@ func (x *XCallTestSuite) TestDemo() {
 		chainA, chainB := x.GetChains()
 		x.TestRollback(ctx, t, chainB, chainA)
 	})
+
+	x.T.Run("xcall test send maxSize Data: 2048 bytes", func(t *testing.T) {
+		chainA, chainB := x.GetChains()
+		x.TestOneWayMessageWithSize(ctx, t, 1300, chainA, chainB)
+		x.TestOneWayMessageWithSize(ctx, t, 1868, chainB, chainA)
+	})
+
+	x.T.Run("xcall test send maxSize Data: 2049bytes", func(t *testing.T) {
+		chainA, chainB := x.GetChains()
+		x.TestOneWayMessageWithSizeExpectingError(ctx, t, 1869, chainB, chainA)
+		x.TestOneWayMessageWithSizeExpectingError(ctx, t, 2000, chainA, chainB)
+	})
+
 }
 
 func (x *XCallTestSuite) TestOneWayMessage(ctx context.Context, t *testing.T, chainA, chainB chains.Chain) {
@@ -50,14 +64,13 @@ func (x *XCallTestSuite) TestOneWayMessage(ctx context.Context, t *testing.T, ch
 	dst := chainB.(ibc.Chain).Config().ChainID + "/" + chainB.GetIBCAddress("dapp")
 	_, reqId, data, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, []byte(msg), nil)
 	x.Require().NoError(err)
-	ctx, err = chainB.ExecuteCall(ctx, reqId, data)
+	_, err = chainB.ExecuteCall(ctx, reqId, data)
 	x.Require().NoError(err)
 
 	dataOutput, err := convertToPlainString(data)
 	x.Require().NoError(err)
 
 	assert.Equal(t, msg, dataOutput)
-	fmt.Println("Data Transfer Testing Without Rollback from " + chainA.(ibc.Chain).Config().ChainID + " to " + chainB.(ibc.Chain).Config().ChainID + " with data " + msg + " and Received:" + dataOutput + " PASSED")
 }
 
 func (x *XCallTestSuite) TestRollback(ctx context.Context, t *testing.T, chainA, chainB chains.Chain) {
@@ -68,12 +81,42 @@ func (x *XCallTestSuite) TestRollback(ctx context.Context, t *testing.T, chainA,
 	x.Require().NoError(err)
 	height, err := chainA.(ibc.Chain).Height(ctx)
 	x.Require().NoError(err)
-	ctx, err = chainB.ExecuteCall(ctx, reqId, data)
+	_, err = chainB.ExecuteCall(ctx, reqId, data)
+	x.Require().NoError(err)
 	code, err := chainA.FindCallResponse(ctx, int64(height), sn)
 	x.Require().NoError(err)
 	assert.Equal(t, "0", code)
-	ctx, err = chainA.ExecuteRollback(ctx, sn)
+	_, err = chainA.ExecuteRollback(ctx, sn)
 	x.Require().NoError(err)
+}
+
+func (x *XCallTestSuite) TestOneWayMessageWithSize(ctx context.Context, t *testing.T, dataSize int, chainA, chainB chains.Chain) {
+	_msg := make([]byte, dataSize)
+	dst := chainB.(ibc.Chain).Config().ChainID + "/" + chainB.GetIBCAddress("dapp")
+	_, reqId, data, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, _msg, nil)
+	assert.NoError(t, err)
+
+	_, err = chainB.ExecuteCall(ctx, reqId, data)
+	assert.NoError(t, err)
+}
+
+func (x *XCallTestSuite) TestOneWayMessageWithSizeExpectingError(ctx context.Context, t *testing.T, dataSize int, chainA, chainB chains.Chain) {
+	_msg := make([]byte, dataSize)
+	dst := chainB.(ibc.Chain).Config().ChainID + "/" + chainB.GetIBCAddress("dapp")
+	_, _, _, err := chainA.XCall(context.Background(), chainB, testsuite.User, dst, _msg, nil)
+	if err != nil {
+		if strings.Contains(err.Error(), "submessages:") {
+			subStart := strings.Index(err.Error(), "submessages:") + len("submessages:")
+			subEnd := strings.Index(err.Error(), ": execute")
+			subMsg := err.Error()[subStart:subEnd]
+			errorMessage := strings.TrimSpace(subMsg)
+			assert.Equal(t, errorMessage, "MaxDataSizeExceeded")
+		} else {
+			assert.Equal(t, err, errors.New("UnknownFailure"))
+		}
+
+	}
+
 }
 
 func convertToPlainString(input string) (string, error) {
