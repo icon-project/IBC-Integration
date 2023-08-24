da9c75f0b6a02f990e0d204870b22ec986599099
sabinchitrakar <immortal.infidel@gmail.com>
2023-08-24 17:17:19 +0545

    fix: add test for send packet

--- contracts/cosmwasm-vm/cw-ibc-core/tests/channel/test_packet.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/tests/channel/test_packet.rs
@@ -70,3 +70,74 @@ fn test_packet_send_fail_invalid_sequence() {
         .send_packet(deps.as_mut(), &mock_env(), packet)
         .unwrap();
 }
+
+#[should_panic(
+    expected = "IbcPacketError { error: FrozenClient { client_id: ClientId(\"default-0\") } }"
+)]
+#[test]
+fn test_packet_send_fails_on_frozen_client() {
+    let contract = CwIbcCoreContext::default();
+    let mut deps = deps();
+    let env = get_mock_env();
+    let timestamp_future = Timestamp::default();
+    let timeout_height_future = 100;
+
+    let packet = get_dummy_raw_packet(timeout_height_future, timestamp_future.nanoseconds());
+
+    let mut test_context = TestContext::for_send_packet(env, &packet);
+    let mut client_state = test_context.client_state.unwrap();
+    client_state.frozen_height = 1000;
+    test_context.client_state = Some(client_state);
+    test_context.init_send_packet(deps.as_mut().storage, &contract);
+
+    contract
+        .send_packet(deps.as_mut(), &mock_env(), packet)
+        .unwrap();
+}
+
+#[should_panic(
+    expected = "IbcPacketError { error: InvalidChannelState { channel_id: ChannelId(\"channel-1\"), state: Init } }"
+)]
+#[test]
+fn test_packet_send_fails_on_invalid_client_state() {
+    let contract = CwIbcCoreContext::default();
+    let mut deps = deps();
+    let env = get_mock_env();
+    let timestamp_future = Timestamp::default();
+    let timeout_height_future = 100;
+
+    let packet = get_dummy_raw_packet(timeout_height_future, timestamp_future.nanoseconds());
+
+    let mut test_context = TestContext::for_send_packet(env, &packet);
+    let mut channel_end = test_context.channel_end();
+    channel_end.state = State::Init;
+    test_context.channel_end = Some(channel_end);
+    test_context.init_send_packet(deps.as_mut().storage, &contract);
+
+    contract
+        .send_packet(deps.as_mut(), &mock_env(), packet)
+        .unwrap();
+}
+
+#[should_panic(
+    expected = "IbcPacketError { error: InvalidPacketCounterparty { port_id: PortId(\"their-port\"), channel_id: ChannelId(\"invalid_channel\") } }"
+)]
+#[test]
+fn test_packet_send_fails_on_invalid_counterparty() {
+    let contract = CwIbcCoreContext::default();
+    let mut deps = deps();
+    let env = get_mock_env();
+    let timestamp_future = Timestamp::default();
+    let timeout_height_future = 100;
+
+    let mut packet = get_dummy_raw_packet(timeout_height_future, timestamp_future.nanoseconds());
+
+    let test_context = TestContext::for_send_packet(env, &packet);
+
+    test_context.init_send_packet(deps.as_mut().storage, &contract);
+    packet.destination_channel = "invalid_channel".to_string();
+
+    contract
+        .send_packet(deps.as_mut(), &mock_env(), packet)
+        .unwrap();
+}
