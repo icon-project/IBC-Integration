4acdc6f4759f008e788f5740d4ba32dec10cdf4b
qwerty0789 <134275268+qwerty0789@users.noreply.github.com>
2023-08-25 14:56:15 +0545

    fix: owner check removed from ibc-handler (#663)

--- contracts/javascore/ibc/src/main/java/ibc/ics25/handler/IBCHandler.java
+++ contracts/javascore/ibc/src/main/java/ibc/ics25/handler/IBCHandler.java
@@ -31,7 +31,6 @@ public class IBCHandler extends IBCHandlerPacket {
 
     @External
     public void createClient(MsgCreateClient msg) {
-        onlyOwner();
         super.createClient(msg);
     }
 
