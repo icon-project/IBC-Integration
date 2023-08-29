304452038b63710f6c9e607f08f8898bf8bd2633
ibrizsabin <101165234+ibrizsabin@users.noreply.github.com>
2023-08-29 13:47:15 +0545

    fix: refactor unnecessary replies (#647)
    * fix: make reply on success
    
    * fix: clear callback data
    
    * fix: chan open init remove reply
    
    * fix: remove reply chan open try
    
    * fix: remove reply from open confirm
    
    * fix: remove close init reply
    
    * fix: fix tests
    
    * fix: add unit tests
    
    * fix: add unit tests
    
    * fix: unit tests
    
    * fix: remove reply on ack
    
    * fix: fix format
    
    * fix: remove receive packet callback
    
    * chore: pass buils
    
    * fix: add tests
    
    * fix: refactor test setup
    
    * fix: cleanup test
    
    * fix: receive test
    
    * fix: refactor test
    
    * chore: pass build
    
    * fix: fix close init test
    
    * fix: add test for send packet
    
    * fix: test for close confirm
    
    * chore: pass build
    
    * fix: cargo fmt
    
    * fix: timeout packet reply never
    
    * fix: fix lint
    
    ---------
    
    Co-authored-by: sabinchitrakar <immortal.infidel@gmail.com>

--- contracts/cosmwasm-vm/cw-ibc-core/src/ics04_channel/packet/timeout.rs
+++ contracts/cosmwasm-vm/cw-ibc-core/src/ics04_channel/packet/timeout.rs
@@ -1,7 +1,4 @@
-use cw_common::{
-    raw_types::{channel::RawMessageTimeout, to_raw_packet},
-    to_checked_address,
-};
+use cw_common::{raw_types::channel::RawMessageTimeout, to_checked_address};
 
 use crate::conversions::{
     to_ibc_channel_id, to_ibc_height, to_ibc_port_id, to_ibc_timeout_block, to_ibc_timeout_height,
@@ -49,8 +46,8 @@ impl<'a> CwIbcCoreContext<'a> {
         let proof_height = to_ibc_height(msg.proof_height.clone())?;
         let next_sequence_recv = Sequence::from(msg.next_sequence_recv);
 
-        let chan_end_on_a = self.get_channel_end(deps.storage, &src_port, &src_channel)?;
-        if !chan_end_on_a.state_matches(&State::Open) {
+        let mut channel_end = self.get_channel_end(deps.storage, &src_port, &src_channel)?;
+        if !channel_end.state_matches(&State::Open) {
             return Err(ContractError::IbcPacketError {
                 error: PacketError::ChannelClosed {
                     channel_id: src_channel,
@@ -58,7 +55,7 @@ impl<'a> CwIbcCoreContext<'a> {
             });
         }
         let counterparty = Counterparty::new(dst_port.clone(), Some(dst_channel.clone()));
-        if !chan_end_on_a.counterparty_matches(&counterparty) {
+        if !channel_end.counterparty_matches(&counterparty) {
             return Err(ContractError::IbcPacketError {
                 error: PacketError::InvalidPacketCounterparty {
                     port_id: dst_port,
@@ -66,23 +63,11 @@ impl<'a> CwIbcCoreContext<'a> {
                 },
             });
         }
-        let conn_id_on_a = chan_end_on_a.connection_hops()[0].clone();
+        let conn_id_on_a = channel_end.connection_hops()[0].clone();
         let conn_end_on_a = self.connection_end(deps.storage, &conn_id_on_a)?;
 
-        let commitment_on_a = match self.get_packet_commitment(
-            deps.storage,
-            &src_port,
-            &src_channel,
-            packet_sequence,
-        ) {
-            Ok(commitment_on_a) => commitment_on_a,
-
-            // This error indicates that the timeout has already been relayed
-            // or there is a misconfigured relayer attempting to prove a timeout
-            // for a packet never sent. Core IBC will treat this error as a no-op in order to
-            // prevent an entire relay transaction from failing and consuming unnecessary fees.
-            Err(_) => return Ok(Response::new()),
-        };
+        let commitment_on_a =
+            self.get_packet_commitment(deps.storage, &src_port, &src_channel, packet_sequence)?;
 
         let expected_commitment_on_a = commitment::compute_packet_commitment(
             &packet.data,
@@ -118,7 +103,7 @@ impl<'a> CwIbcCoreContext<'a> {
         )?;
 
         let next_seq_recv_verification_result: LightClientPacketMessage =
-            if chan_end_on_a.order_matches(&Order::Ordered) {
+            if channel_end.order_matches(&Order::Ordered) {
                 if packet_sequence < next_sequence_recv {
                     return Err(ContractError::IbcPacketError {
                         error: PacketError::InvalidPacketSequence {
@@ -173,109 +158,48 @@ impl<'a> CwIbcCoreContext<'a> {
         let timeoutblock = to_ibc_timeout_block(&packet_timeout_height);
         let timeout = CwTimeout::with_block(timeoutblock);
         let ibc_packet = CwPacket::new(data, src, dest, packet.sequence, timeout);
-        self.store_callback_data(
+
+        self.delete_packet_commitment(
             deps.storage,
-            VALIDATE_ON_PACKET_TIMEOUT_ON_MODULE,
-            &ibc_packet,
+            &src_port,
+            &src_channel,
+            packet.sequence.into(),
+        )?;
+
+        if let Order::Ordered = channel_end.ordering {
+            channel_end.state = State::Closed;
+            self.store_channel_end(deps.storage, &src_port, &src_channel, &channel_end)?;
+        }
+
+        let event = create_packet_event(
+            IbcEventType::Timeout,
+            packet,
+            channel_end.ordering(),
+            &conn_id_on_a,
+            None,
         )?;
 
         let address = to_checked_address(deps.as_ref(), &msg.signer);
         let cosm_msg = cw_common::xcall_connection_msg::ExecuteMsg::IbcPacketTimeout {
             msg: cosmwasm_std::IbcPacketTimeoutMsg::new(ibc_packet, address),
         };
-        let create_client_message: CosmosMsg = CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
+        let timeout_message: CosmosMsg = CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
             contract_addr: contract_address,
             msg: to_binary(&cosm_msg).unwrap(),
             funds: info.funds,
         });
-        let sub_msg: SubMsg =
-            SubMsg::reply_on_success(create_client_message, VALIDATE_ON_PACKET_TIMEOUT_ON_MODULE);
+
+        let sub_msg = SubMsg {
+            id: VALIDATE_ON_PACKET_TIMEOUT_ON_MODULE,
+            msg: timeout_message,
+            gas_limit: None,
+            reply_on: cosmwasm_std::ReplyOn::Never,
+        };
 
         Ok(Response::new()
             .add_attribute("action", "packet")
             .add_attribute("method", "packet_timeout_module_validation")
-            .add_submessage(sub_msg))
-    }
-
-    /// This function handles the execution of a timeout packet after successfull validation of
-    /// light client and xcall.
-    ///
-    /// Arguments:
-    ///
-    /// * `deps`: `deps` is a mutable reference to the dependencies of the contract, which includes
-    /// access to the storage and other modules.
-    /// * `message`: `message` is a `Reply` struct that contains the result of a sub-message sent by the
-    /// contract to another module. It is used to process the result of an IBC packet timeout.
-    ///
-    /// Returns:
-    ///
-    /// a `Result<Response, ContractError>` where `Response` is a struct representing the response to a
-    /// contract execution and `ContractError` is an enum representing the possible errors that can
-    /// occur during contract execution.
-    pub fn execute_timeout_packet(
-        &self,
-        deps: DepsMut,
-        message: Reply,
-    ) -> Result<Response, ContractError> {
-        match message.result {
-            cosmwasm_std::SubMsgResult::Ok(_res) => {
-                let packet: CwPacket = self.get_callback_data(
-                    deps.as_ref().storage,
-                    VALIDATE_ON_PACKET_TIMEOUT_ON_MODULE,
-                )?;
-                let channel_id = IbcChannelId::from_str(&packet.src.channel_id).unwrap();
-                let port_id = IbcPortId::from_str(&packet.src.port_id).unwrap();
-                let chan_end_on_a = self.get_channel_end(deps.storage, &port_id, &channel_id)?;
-                if self
-                    .get_packet_commitment(
-                        deps.storage,
-                        &port_id,
-                        &channel_id,
-                        packet.sequence.into(),
-                    )
-                    .is_err()
-                {
-                    return Ok(Response::new());
-                }
-                self.delete_packet_commitment(
-                    deps.storage,
-                    &port_id,
-                    &channel_id,
-                    packet.sequence.into(),
-                )?;
-                let chan_end_on_a = {
-                    if let Order::Ordered = chan_end_on_a.ordering {
-                        let mut chan_end_on_a = chan_end_on_a;
-                        chan_end_on_a.state = State::Closed;
-                        self.store_channel_end(
-                            deps.storage,
-                            &port_id,
-                            &channel_id,
-                            &chan_end_on_a,
-                        )?;
-
-                        chan_end_on_a
-                    } else {
-                        chan_end_on_a
-                    }
-                };
-
-                let conn_id_on_a = &chan_end_on_a.connection_hops()[0];
-
-                let event = create_packet_event(
-                    IbcEventType::Timeout,
-                    to_raw_packet(packet),
-                    chan_end_on_a.ordering(),
-                    conn_id_on_a,
-                    None,
-                )?;
-
-                Ok(Response::new()
-                    .add_attribute("action", "packet")
-                    .add_attribute("method", "execute_timeout_packet")
-                    .add_event(event))
-            }
-            cosmwasm_std::SubMsgResult::Err(e) => Err(ContractError::IbcContextError { error: e }),
-        }
+            .add_submessage(sub_msg)
+            .add_event(event))
     }
 }
