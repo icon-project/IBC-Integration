d1c8051cbc53bcebca0f0000ae72d65af6d497d0
gcranju <134275268+gcranju@users.noreply.github.com>
2023-09-11 17:57:10 +0545

    feat: cw-mock-ibc-connection added (#711)
    * feat/cw-mock-ibc-connection added
    
    * feat: mock xcall connection added
    
    * fix: cargo lints fixes
    
    * fix: mock xcall connection functionality reduced
    
    * fix: cargo lints fixes

---
+++ contracts/cosmwasm-vm/cw-mock-ibc-connection/src/admin.rs
@@ -0,0 +1,37 @@
+use cosmwasm_std::{MessageInfo, Response, Storage};
+
+use crate::{error::ContractError, state::CwIbcConnection};
+
+impl<'a> CwIbcConnection<'a> {
+    pub fn query_admin(&self, store: &dyn Storage) -> Result<String, ContractError> {
+        let admin = self
+            .admin()
+            .load(store)
+            .map_err(|_| ContractError::AdminNotExist)?;
+
+        Ok(admin)
+    }
+    pub fn add_admin(
+        &self,
+        store: &mut dyn Storage,
+        info: MessageInfo,
+        admin: String,
+    ) -> Result<Response, ContractError> {
+        if admin.is_empty() {
+            return Err(ContractError::AdminAddressCannotBeNull {});
+        }
+
+        let owner = self
+            .owner()
+            .load(store)
+            .map_err(|_| ContractError::Unauthorized {})?;
+
+        if info.sender != owner {
+            return Err(ContractError::Unauthorized {});
+        }
+        self.admin().save(store, &admin)?;
+        Ok(Response::new()
+            .add_attribute("method", "add_admin")
+            .add_attribute("admin", admin.to_string()))
+    }
+}
