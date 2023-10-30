eba3565c04995581dc11c9c432ba362c2560a4b6
viveksharmapoudel <viveksharmapoudel@gmail.com>
2023-10-30 18:05:34 +0545

    fix: using constant timestamp value on icon

--- libraries/go/common/icon/icon_consensus_extended.go
+++ libraries/go/common/icon/icon_consensus_extended.go
@@ -2,4 +2,4 @@ package icon
 
 func (m *ConsensusState) ValidateBasic() error { return nil }
 func (m *ConsensusState) ClientType() string   { return "icon" }
-func (m *ConsensusState) GetTimestamp() uint64 { return 0 }
+func (m *ConsensusState) GetTimestamp() uint64 { return 1 }
--- libraries/go/common/tendermint/consensus_state_extended.go
+++ libraries/go/common/tendermint/consensus_state_extended.go
@@ -1,5 +1,5 @@
 package tendermint
 
 func (m *ConsensusState) ValidateBasic() error { return nil }
-func (m *ConsensusState) ClientType() string   { return "icon" }
-func (m *ConsensusState) GetTimestamp() uint64 { return 1 }
+func (m *ConsensusState) ClientType() string   { return "07-tendermint" }
+func (m *ConsensusState) GetTimestamp() uint64 { return 0 }
