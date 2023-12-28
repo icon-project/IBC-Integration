ee328eb5b7e4c4d39612bd0a883fe17312ce0802
sdpbhandari <bhandari.sudeep68@gmail.com>
2023-12-28 19:00:33 +0545

    fix: deploy of the contract

--- contracts/javascore/modules/ics20app/src/main/java/ibc/ics20app/ICS20TransferBank.java
+++ contracts/javascore/modules/ics20app/src/main/java/ibc/ics20app/ICS20TransferBank.java
@@ -7,28 +7,29 @@ import score.VarDB;
 import score.annotation.External;
 
 import java.math.BigInteger;
-import java.util.Arrays;
 
 public class ICS20TransferBank extends ICS20Transfer {
-    private final VarDB<Address> ibcHandler = Context.newVarDB("ibcHandler", Address.class);
-    private final VarDB<Address> bank = Context.newVarDB("bank", Address.class);
+    public static final VarDB<Address> ibcHandler = Context.newVarDB("ibcHandler", Address.class);
+    public final VarDB<Address> bank = Context.newVarDB("bank", Address.class);
 
-    public ICS20TransferBank(Address ibcHandler, Address bank) {
-        this.ibcHandler.set(ibcHandler);
-        this.bank.set(bank);
+    public ICS20TransferBank(Address _ibcHandler, Address _bank) {
+        if (ibcHandler.get() == null) {
+            ibcHandler.set(_ibcHandler);
+            this.bank.set(_bank);
+        }
     }
 
     @External
     public void sendTransfer(String denom, BigInteger amount, String receiver, String sourcePort, String sourceChannel, BigInteger timeoutHeight) {
         byte[] denomPrefix = ICS20Transfer.getDenomPrefix(sourcePort, sourceChannel);
         Address caller = Context.getCaller();
-        if (!denom.startsWith(Arrays.toString(denomPrefix))) {
+        if (!denom.startsWith(denomPrefix.toString())) {
             Context.require(_transferFrom(caller, ICS20Transfer.getEscrowAddress(sourceChannel), denom, amount), "transfer failed");
-        }else {
+        } else {
             Context.require(_burn(caller, denom, amount), "burn failed");
         }
 
-        byte[] packetData = ICS20Lib.marshalJson(denom,amount, ICS20Transfer._encodeSender(caller),receiver);
+        byte[] packetData = ICS20Lib.marshalJson(denom, amount, ICS20Transfer._encodeSender(caller), receiver);
         Height height = new Height();
         height.setRevisionNumber(BigInteger.ZERO);
         height.setRevisionHeight(timeoutHeight);
