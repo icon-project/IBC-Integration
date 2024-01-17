0356f8ae9e65867ac723108b69f4d445185c0847
izyak <lbackup887@gmail.com>
2024-01-17 16:39:30 +0545

    feat: implement get timestamp for tendermint

--- libraries/go/common/tendermint/consensus_state_extended.go
+++ libraries/go/common/tendermint/consensus_state_extended.go
@@ -1,5 +1,12 @@
 package tendermint
 
+import "time"
+
 func (m *ConsensusState) ValidateBasic() error { return nil }
-func (m *ConsensusState) ClientType() string   { return "icon" }
-func (m *ConsensusState) GetTimestamp() uint64 { return 0 }
+func (m *ConsensusState) ClientType() string   { return "icAon" }
+
+// this returns the timestamp in nanoseconds
+func (m *ConsensusState) GetTimestamp() uint64 {
+	secondsToNano := time.Second * time.Duration(m.Timestamp.Seconds) // convert seconds to nanoseconds
+	return uint64(secondsToNano + time.Duration(m.Timestamp.Nanos))
+}
