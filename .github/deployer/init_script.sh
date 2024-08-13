266fc51aa8f115cd959355dbbcc5dd6f4c5d2a6a
DeepakBomjan <44976635+DeepakBomjan@users.noreply.github.com>
2024-08-13 11:51:55 +0545

    ci: update archwayd version to 7.0.0 (#879)
    * ci: update archwayd version to 7.0.0
    
    * ci: minor correction

--- .github/deployer/init_script.sh
+++ .github/deployer/init_script.sh
@@ -13,9 +13,10 @@ KMS_ID="KMS_ID_HERE"
 DEPLOYR_HOME="/home/deployr"
 GO_VERS="1.20.6"
 JAVA_VERS="11.0.18_10"
-ARCHWAY_VERS="6.0.0"
+ARCHWAY_VERS="7.0.0"
 INJECTIVE_VERS="1.12.1-1705909076"
 NEUTRON_VERS="3.0.2"
+SUI_VERS="mainnet-v1.25.1"
 
 set -x
 export GOROOT=/usr/local/go
@@ -135,8 +136,8 @@ tar xf OpenJDK11U-jdk_x64_linux_hotspot_$${JAVA_VERS}.tar.gz -C /opt/java
 go install github.com/icon-project/goloop/cmd/goloop@latest
 
 # Install archway
-wget -q https://github.com/archway-network/archway/releases/download/v$${ARCHWAY_VERS}/archway_$${ARCHWAY_VERS}_linux_amd64.zip
-unzip archway_$${ARCHWAY_VERS}_linux_amd64.zip
+wget -q https://github.com/archway-network/archway/releases/download/v$${ARCHWAY_VERS}/archwayd_$${ARCHWAY_VERS}_linux_amd64.zip
+unzip archwayd_$${ARCHWAY_VERS}_linux_amd64.zip
 sudo cp archwayd /usr/local/bin
 
 # Install injectived
