0bedf2ebffa5f2491fb040e2ab9934d993621cba
Deepak Bomjan <dpaktamang@gmail.com>
2024-08-15 14:57:22 +0545

    ci: add sui key import script

---
+++ .github/deployer/sui_import.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ARCHWAY_KEY=$(archwayd keys export ibc_wallet --unsafe --unarmored-hex --keyring-backend test)
+SUI_KEY=$(sui keytool convert $ARCHWAY_KEY --json | grep suiprivkey | awk -F\" '{print $4}')
+rm -rf /root/.sui/sui_config/*
+sui keytool import --alias sui_wallet_testnet $SUI_KEY ed25519 >/dev/null && echo "SUCCESS" || echo "FAILED"
